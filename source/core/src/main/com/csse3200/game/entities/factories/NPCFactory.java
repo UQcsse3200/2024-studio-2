d2ecf82fafb8fc1190dc0647053681255e6d30dd
Bailey Stoodley <Bailey@bunnycup.com>
2024-08-12 09:31:36 +1000

    Removed a chase interest in the player for the friendly NPCS

--- source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
@@ -126,8 +126,7 @@ public class NPCFactory {
   private static Entity createFriendlyBaseNPC(Entity target) {
     AITaskComponent aiComponent =
             new AITaskComponent()
-                    .addTask(new WanderTask(new Vector2(2f, 2f), 2f))
-                    .addTask(new ChaseTask(target, 10, 3f, 4f));
+                    .addTask(new WanderTask(new Vector2(2f, 2f), 2f));
     Entity npc =
             new Entity()
                     .addComponent(new PhysicsComponent())
