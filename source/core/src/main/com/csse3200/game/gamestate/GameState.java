44dcf3235fff8949b95993b2e940d29d85ab2a8a
Lachlan Ellis <37792607+lellis0406@users.noreply.github.com>
2024-08-11 19:13:11 +1000

    added javadoc documentation for gamestate and savehandler

--- source/core/src/main/com/csse3200/game/gamestate/GameState.java
+++ source/core/src/main/com/csse3200/game/gamestate/GameState.java
@@ -1,5 +1,8 @@
 package com.csse3200.game.gamestate;
 
+/**
+ * A data struct that contains other data structs to be saved to JSONs by SaveHandler.
+ */
 public class GameState {
     public static EnvironmentVariables env = new EnvironmentVariables();
 }
