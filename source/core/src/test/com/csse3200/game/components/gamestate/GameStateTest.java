f443534ecdcfa4609f43f618ead10a25a86b0719
Lachlan Ellis <37792607+lellis0406@users.noreply.github.com>
2024-08-11 18:28:08 +1000

    Added save/load unit tests

---
+++ source/core/src/test/com/csse3200/game/components/gamestate/GameStateTest.java
@@ -0,0 +1,6 @@
+package com.csse3200.game.components.gamestate;
+
+class GameStateTest {
+    protected static EnvironmentVariablesTest env1 = new EnvironmentVariablesTest();
+    protected static EnvironmentVariablesTest env2 = new EnvironmentVariablesTest();
+}
