9e1cfba1659b3d5f6552973b53a80a1018258420
shalomi <s.panaduraarachchige@student.uq.edu.au>
2024-08-12 20:43:37 +1000

    Initial food commit #3 #28

---
+++ source/core/src/main/com/csse3200/game/inventory/items/Food.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.inventory.items;
+
+public class Food extends ConsumableItems {
+    /**
+     * Constructs a ConsumableItems with the specified uses
+     *
+     * @param quantity
+     */
+    public Food(int quantity) {
+        super(quantity);
+    }
+}
