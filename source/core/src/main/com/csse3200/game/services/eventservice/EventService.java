d8058304e4a8a9ecb3ef69d1c245b7d95f7e385e
noahxl234 <noah@xelleron.com>
2024-08-12 18:03:53 +1000

    Added createEvent methods to EventService

--- source/core/src/main/com/csse3200/game/services/eventservice/EventService.java
+++ source/core/src/main/com/csse3200/game/services/eventservice/EventService.java
@@ -1,8 +1,9 @@
 package com.csse3200.game.services.eventservice;
 
 import com.csse3200.game.events.EventHandler;
-
+import com.csse3200.game.events.listeners.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class EventService {
     public ArrayList<GlobalEvent> globalEvents;
@@ -12,15 +13,38 @@ public class EventService {
         globalEvents = new ArrayList<>();
         globalEventHandler = new EventHandler();
         // Events should Load from config
-        globalEvents.add(new GlobalEvent("steps", 0));
+        //example loading basic step event
+        globalEvents.add(new GlobalEvent("steps"));
         for(GlobalEvent event : globalEvents) {
             globalEventHandler.addListener(event.eventName, this::recordEvent);
         }
     }
 
-    public void recordEvent(int questId) {
+    /*
+    createEvent adds a new event to the Eventhandler and sets a function to run on its triggering.
+    eventName is the name of the event
+    questId
+    listener is the listener function that is called when the event is triggered
+     */
+    public void createEvent(String eventName, EventListener0 listener) {
+        globalEvents.add(new GlobalEvent(eventName));
+        globalEventHandler.addListener(eventName, listener);
+    }
+
+    public <T> void createEvent(String eventName, EventListener1<T> listener) {
+        globalEvents.add(new GlobalEvent(eventName));
+        globalEventHandler.addListener(eventName, listener);
+    }
+
+    public <T0, T1> void createEvent(String eventName, EventListener2<T0, T1> listener) {
+        globalEvents.add(new GlobalEvent(eventName));
+        globalEventHandler.addListener(eventName, listener);
+    }
+
+
+    public void recordEvent(String eventName) {
         for (GlobalEvent globalEvent : this.globalEvents) {
-            if (globalEvent.questId == questId) {
+            if (Objects.equals(globalEvent.eventName, eventName)) {
                 // Can re-add listener here for multiple events
                 globalEvent.recordEvent();
             }
--- source/core/src/main/com/csse3200/game/services/eventservice/GlobalEvent.java
+++ source/core/src/main/com/csse3200/game/services/eventservice/GlobalEvent.java
@@ -8,20 +8,18 @@ import java.util.Objects;
 
 public class GlobalEvent  {
     public String eventName;
-    public Integer questId;
     private static final Logger logger = LoggerFactory.getLogger(GlobalEvent.class);
 
     /*
     Constructor
      */
-    public GlobalEvent(String eventName, Integer questId) {
+    public GlobalEvent(String eventName) {
         this.eventName = eventName;
-        this.questId = questId;
     }
 
     public void recordEvent() {
         // Send trigger to quest here accessing information about this GlobalEvent
-        // e.g. QuestManager.getInstance().QuestHandler.trigger(this.questId,this.eventName);
+        // e.g. QuestManager.getInstance().QuestHandler.trigger(this.eventName);
         // will send a trigger with the eventName as the argument to questId listener in an event handler called QuestHandler in the QuestManager singleton class
         if (Objects.equals(this.eventName, "steps")) {
             logger.info("Steps Event Recorded");
