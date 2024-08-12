a2addac98e3f0eba40a69cadec1a6ef6e174caf8
Phong Nguyen <106220789+PHONGNGYUEN1@users.noreply.github.com>
2024-08-12 21:39:03 +1000

    Created ConsumableItem abstract class

---
+++ source/core/src/main/com/csse3200/game/inventory/items/ConsumableItem.java
@@ -0,0 +1,43 @@
+package main.com.csse3200.game.inventory.items;
+
+import com.csse3200.game.inventory.items.ItemUsageContext;
+import com.csse3200.game.inventory.items.ItemInterface;
+
+/**
+ * Base class for consumable type items that can be used by a player.
+ * <p>
+ * This class provides a common implementation for the {@link ItemInterface} interface,
+ * including default behavior for common properties like isConsumed and quantity. Subclasses
+ * must provide their own implementation for the {@code useItem} method, which
+ * defines how the item can be used as well how many uses is left and must define what {@link ItemUsageContext}
+ * should be input to {@code useItem}.
+ * </p>
+ */
+public abstract class ConsumableItem extends AbstractItem {
+    protected boolean isConsumed;
+
+    /**
+     * Constructs a ConsumableItems with the specified uses
+     */
+    public ConsumableItem(String name, int quantity, int limit, boolean isConsumed) {
+        super(name, limit, quantity);
+        this.isConsumed = isConsumed; // Flag iff there is no consumables uses left (true if not added or empty)
+    }
+
+    /**
+     *  Updates the no. of uses left for each usage
+     *
+     * @param inputs the inputs for this ite to be used
+     */
+    @Override
+    public void useItem(ItemUsageContext inputs) {
+        if (super.quantity > 0) {
+            super.quantity--;
+            this.isConsumed = false;
+            //Write functionality to how it would be used
+        }
+        else {
+            this.isConsumed = true;
+        }
+    }
+}
---
+++ source/core/src/test/com/csse3200/game/inventory/items/ConsumableItemTest.java
@@ -0,0 +1,2 @@
+package test.com.csse3200.game.inventory.items;public class ConsumableItemTest {
+}
