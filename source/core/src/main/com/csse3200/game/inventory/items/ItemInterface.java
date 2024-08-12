9971ba098b6b6d86a90f278667576d8025218cbe
shalomi <s.panaduraarachchige@student.uq.edu.au>
2024-08-12 20:43:21 +1000

    Base commit #3 #28

---
+++ source/core/src/main/com/csse3200/game/inventory/items/ItemInterface.java
@@ -0,0 +1,53 @@
+package com.csse3200.game.inventory.items;
+
+/**
+ * Represents an item that can be used by a player and stored within the inventory.
+ */
+public interface ItemInterface {
+    /**
+     * Gets the name of the item.
+     *
+     * @return the name of the item
+     */
+    String getName();
+
+    /**
+     * Gets the unique item code (up to item name) for the item
+     */
+    int getItemCode();
+
+    /**
+     * Gets the maximum number of this item that can be held in a stack.
+     *
+     * @return the limit of the item
+     */
+    int getLimit();
+
+    /**
+     * @return the quantity of the item
+     */
+    int getQuantity();
+
+    /**
+     * Uses the item with the provided context or argument and returns a result.
+     *
+     * @param inputs the inputs required to use the item
+     */
+    void useItem(ItemUsageContext inputs);
+
+    /**
+     * @return whether the quantity of this item can be increased by n
+     */
+    boolean canAdd(int n);
+
+    /**
+     * @param n - number of item to add
+     */
+    void add(int n);
+
+    /**
+     * Checks whether an item has been consumed.
+     * @return whether an item has been consumed.
+     */
+    boolean isConsumed();
+}
