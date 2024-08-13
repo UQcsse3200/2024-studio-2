7f3c5f920d365c6e78577ee18d8618e4f01f3db9
Phong Nguyen <106220789+PHONGNGYUEN1@users.noreply.github.com>
2024-08-13 23:21:13 +1000

    Implemented review comments for ConsumableItem class and test

--- source/core/src/main/com/csse3200/game/inventory/items/ConsumableItem.java
+++ source/core/src/main/com/csse3200/game/inventory/items/ConsumableItem.java
@@ -4,7 +4,7 @@ package com.csse3200.game.inventory.items;
  * Base class for consumable type items that can be used by a player.
  * <p>
  * It is items that have limited uses until it is depleted (removed) from the inventory
- * Defines the useItemm function in {@link ItemUsageContext}
+ * Defines the useItem function in {@link ItemUsageContext}
  * Should be input to {@code useItem}.
  * </p>
  */
@@ -12,24 +12,28 @@ public abstract class ConsumableItem extends AbstractItem {
 
     /**
      * Constructs a ConsumableItem
+     *
+     * @param name is of item inherited from class AbstractItem
+     * @param itemCode is the itemCode of the item inherited from class AbstractItem
+     * @param limit is the limit of the item inherited from class AbstractItem
+     * @param  quantity is the no. of copies the item has left which is inherited from class AbstractItem
      */
     public ConsumableItem(String name, int itemCode, int limit, int quantity) {
         super(name, itemCode, limit, quantity);
     }
 
     /**
-     *  Updates the no. of uses left for each usage
+     *  Updates the no. of uses left for each usage by decreasing quantity count after each call
      *
      * @param inputs the inputs for this ite to be used
+     * @throws IllegalStateException iff consumable item is attempted to be used with quantity equal to zero (isEmpty())
      */
     @Override
     public void useItem(ItemUsageContext inputs) {
         if (super.isEmpty()) {
-            throw new ArrayIndexOutOfBoundsException("This item has ran out of uses");
+            throw new IllegalStateException("This item has ran out of uses");
         }
-        else {
-            this.quantity--;
-            super.isEmpty();
-        }
+        this.quantity--;
+        super.isEmpty();
     }
 }
--- source/core/src/test/com/csse3200/game/inventory/items/ConsumableItemTest.java
+++ source/core/src/test/com/csse3200/game/inventory/items/ConsumableItemTest.java
@@ -1,72 +1,53 @@
 package com.csse3200.game.inventory.items;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.internal.matchers.text.ValuePrinter.print;
 
 import com.csse3200.game.extensions.GameExtension;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(GameExtension.class)
 public class ConsumableItemTest {
-    private TestableItem1 item1;
-    private TestableItem1 item2;
+    private TestableItem item1;
     private static ItemUsageContext context;
 
     /**
      * Concrete subclass of ConsumableItems for testing purposes.
      */
-    private static class TestableItem1 extends ConsumableItem {
-        public int numUsed = 0;
+    private static class TestableItem extends ConsumableItem {
 
-        public TestableItem1(String name, int itemCode, int limit, int quantity) {
+        public TestableItem(String name, int itemCode, int limit, int quantity) {
             super(name, itemCode, limit, quantity);
-        }
-
-        @Override
-        public void useItem(ItemUsageContext inputs) {
-            //for testing
-            if (super.isEmpty()) {
-                throw new ArrayIndexOutOfBoundsException("This item has ran out of uses");
-            }
-            else {
-                this.quantity--;
-                super.isEmpty();
-            }
         }
     }
 
     @BeforeEach
     void setUp() { // Initialize TestableItem and ItemUsageContext
-        item1 = new TestableItem1("Test1", 3, 10, 3);
-        item2 = new TestableItem1("Test2", 3, 10, 4);
+        item1 = new TestableItem("Test1", 3, 10, 3);
         context = new ItemUsageContext();
     }
 
     @Test
     void testIfConsumableItemIsUsed() {
         String msg = "Checking if consumable item is used: ";
-        int originalQuantity = item2.getQuantity();
+        int originalQuantity = item1.getQuantity();
 
-        item2.useItem(context);
-        assertNotEquals(originalQuantity, item2.quantity);
+        item1.useItem(context);
+        assertNotEquals(originalQuantity, item1.getQuantity());
     }
 
     @Test
-    void testIfEmpty() {
+    void testConsumableItemUsedThrowsError() {
         String msg = "Checking if consumable item is empty: ";
-        int originalQuantity = item1.getQuantity();
 
         item1.useItem(context);
         item1.useItem(context);
         item1.useItem(context);
         assertTrue(item1.isEmpty(), msg + "is empty is true");
 
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> item1.useItem(context));
+        assertThrows(IllegalStateException.class, () -> item1.useItem(context));
     }
 }
