0609fba823e0f88271f351f372d61cbf3b1e52c9
mitsudz <MittunS2003@gmail.com>
2024-08-14 19:59:46 +1000

    Refactor with helper functions #3 #19

--- source/core/src/main/com/csse3200/game/inventory/InventoryInterface.java
+++ source/core/src/main/com/csse3200/game/inventory/InventoryInterface.java
@@ -3,9 +3,14 @@ package com.csse3200.game.inventory;
 import com.csse3200.game.inventory.items.AbstractItem;
 import com.csse3200.game.inventory.items.ItemUsageContext;
 
+import java.util.Optional;
+
+// TODO : Add javadocs for interface!
 /**
  * Represents player's inventory. Can store a certain number of distinct items (determined by
  * limit), some items can be stacked (ie, stackable items).
+ *
+ * <p><b>Note:</b> this interface in future can possibly be abstracted further and used as a </p>
  */
 public interface InventoryInterface {
     int getCapacity(); // How many distinct item slots the inventory has
@@ -13,7 +18,7 @@ public interface InventoryInterface {
     boolean isFull();
     boolean hasItem(int itemCode); // check if inventory has item by code
     int getIndex(int itemCode); // get an index for an item by code
-    AbstractItem getAt(int index); // Get the item at the specified index
+    Optional<AbstractItem> getAt(int index); // Get the item at the specified index
     void deleteItem(int itemCode); // delete any of a particular item by code
     void deleteItemAt(int index); // delete item by inventory index
     void clearInventory(); // Warning - clears entire inventory!
