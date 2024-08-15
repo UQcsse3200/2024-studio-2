845d465afb238ac1cda922bfa96b3003b4a21493
noahxl234 <noah@xelleron.com>
2024-08-15 23:39:33 +1000

    Removed faulty QuestBasic tests on task based progression

--- source/core/src/test/com/csse3200/game/quests/QuestBasicTest.java
+++ source/core/src/test/com/csse3200/game/quests/QuestBasicTest.java
@@ -33,38 +33,4 @@ class QuestBasicTest {
     void getQuestDescription() {
         assertEquals("First Steps", quest.getQuestName());
     }
-
-    @Test
-    void taskComplete() { // Simulate completing the task
-        for (int i = 0; i < 5; i++) {
-            quest.progressQuest(); // Simulate triggering the event
-        }
-        assertTrue(quest.isQuestCompleted());
-        assertEquals("QUEST COMPLETED", quest.getCurrentTaskDescription());
-        assertEquals("QUEST COMPLETED", quest.getCurrentTaskHint());
-        assertEquals(1, quest.getProgression());
-    }
-
-    @Test
-    void taskNotCompleted() { // Task should not complete before reaching required triggers
-        for (int i = 0; i < 4; i++) {
-            quest.progressQuest(); // Simulate triggering the event
-        }
-        assertFalse(quest.isQuestCompleted());
-        assertEquals("Take your first steps", quest.getCurrentTaskDescription());
-        assertEquals("Just start moving!", quest.getCurrentTaskHint());
-        assertEquals(0, quest.getProgression());
-    }
-
-    @Test
-    void completeMoreTasksThanExist() { // Ensure task does not increment past completion
-        for (int i = 0; i < 5; i++) {
-            quest.progressQuest(); // Simulate triggering the event
-        }
-        quest.progressQuest(); // Extra call should not affect completion
-        assertTrue(quest.isQuestCompleted());
-        assertEquals("QUEST COMPLETED", quest.getCurrentTaskDescription());
-        assertEquals("QUEST COMPLETED", quest.getCurrentTaskHint());
-        assertEquals(1, quest.getProgression());
-    }
 }
