ad34052a0b414d8fb9ee94d33292774ebaf42b06
Phong Nguyen <106220789+PHONGNGYUEN1@users.noreply.github.com>
2024-08-16 12:39:33 +1000

    Updated Abstract Food parameter of feedingEffect #3 #28

--- source/core/src/main/com/csse3200/game/inventory/items/food/AbstractFood.java
+++ source/core/src/main/com/csse3200/game/inventory/items/food/AbstractFood.java
@@ -3,6 +3,7 @@ package com.csse3200.game.inventory.items.food;
 import com.csse3200.game.inventory.items.ConsumableItem;
 import com.csse3200.game.inventory.items.ItemUsageContext;
 import com.csse3200.game.inventory.items.effects.AbstractEffect;
+import com.csse3200.game.inventory.items.effects.feeding.FeedEffect;
 import com.csse3200.game.inventory.items.exceptions.ConsumedException;
 
 public class AbstractFood extends ConsumableItem {
@@ -19,7 +20,7 @@ public class AbstractFood extends ConsumableItem {
      * @param limit the stack limit of the item
      * @param quantity the initial quantity for this item
      */
-    public AbstractFood(String name, int itemCode, int limit, int quantity, AbstractEffect feedingEffect) {
+    public AbstractFood(String name, int itemCode, int limit, int quantity, FeedEffect feedingEffect) {
         super(name, itemCode, limit, quantity);
         this.feedingEffect = feedingEffect;
     }
@@ -28,14 +29,14 @@ public class AbstractFood extends ConsumableItem {
      * Returns the effect of the food
      */
     public AbstractEffect getFeedingEffect() {
-        return feedingEffect;
+        return this.feedingEffect;
     }
 
     /**
      * Applies the effects of the food. This method will be used by all food types
      */
     public void applyFeedingEffect() {
-        feedingEffect.apply();
+        this.feedingEffect.apply();
     }
 
     /**
