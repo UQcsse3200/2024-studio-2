fb1d0b7bf25d789f3ff106dd15904025d468d124
Sloth <long240201@gmail.com>
2024-08-16 13:02:22 +1000

    Fix the setting menu
    The setting menu can be opened in a window now instead of transitioning to a new screen

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -1,10 +1,14 @@
 package com.csse3200.game.components.mainmenu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -13,7 +17,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.components.settingsmenu.SettingsMenuDisplay;
 import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +32,7 @@ public class MainMenuDisplay extends UIComponent {
     private static final Logger logger = LoggerFactory.getLogger(MainMenuDisplay.class);
     private static final float Z_INDEX = 2f;
     private Table table;
+    private SettingsMenuDisplay settingsMenuDisplay;
 
     @Override
     public void create() {
@@ -36,6 +44,8 @@ public class MainMenuDisplay extends UIComponent {
         table = new Table();
         table.setFillParent(true);
 
+
+        Table settingMenu = new Table();
         Image title = new Image(ServiceLocator.getResourceService().getAsset("images/box_boy_title.png", Texture.class));
 
         TextButton startBtn = new TextButton("Start", skin);
@@ -71,7 +81,8 @@ public class MainMenuDisplay extends UIComponent {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
                 logger.debug("Settings button clicked");
-                entity.getEvents().trigger("settings");
+                settingMenu.setVisible(!settingMenu.isVisible());
+                table.setTouchable(Touchable.disabled);
             }
         });
 
@@ -87,7 +98,6 @@ public class MainMenuDisplay extends UIComponent {
                 logger.debug("Fullscreen toggled: " + !isFullscreen);
             }
         });
-
         addExitConfirmation(exitBtn);
 
         table.add(title).padTop(50f).padBottom(50f);
@@ -105,11 +115,91 @@ public class MainMenuDisplay extends UIComponent {
         table.add(versionLabel).padTop(20f);
 
         stage.addActor(table);
+
+        makeSettingMenu(settingMenu);
     }
 
     /**
      * Adds an elevation effect to buttons when hovered over.
      */
+
+
+    private void makeSettingMenu(Table settingMenu) {
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE); // Set color to white
+        pixmap.fill();
+
+        // Create a Drawable from the Pixmap
+        Drawable backgroundDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(pixmap)));
+
+        // Dispose of the Pixmap after creating the texture
+        pixmap.dispose();
+
+        int screenWidth = (int) stage.getWidth();
+        int screenHeight = (int) stage.getHeight();
+
+        settingMenu.setSize(550, 350);
+        settingMenu.setBackground(backgroundDrawable);
+        settingMenu.setVisible(false);
+
+        Table topTable = new Table();
+        topTable.top().padTop(10);
+
+        Label title = new Label("Settings", skin, "title");
+        //settingMenu.row().expandY(); // Ensure title is at the top
+
+        topTable.add(title).expandX().center();
+        topTable.row();
+
+        TextButton closeButton = new TextButton("X", skin);
+        topTable.add(closeButton).size(40, 40).right().padRight(10).padTop(-40);
+
+        settingsMenuDisplay = new SettingsMenuDisplay();
+        Table contentTable = settingsMenuDisplay.makeSettingsTable();
+
+        // Create a table for the "Apply" button
+        Table bottomRightTable = new Table();
+        bottomRightTable.bottom(); // Align contents to bottom-right
+
+        TextButton applyButton = new TextButton("Apply", skin);
+        bottomRightTable.add(applyButton).size(80, 40).padBottom(10f).padRight(10f);
+
+        settingMenu.add(topTable).expandX().fillX(); // Top-right table
+        settingMenu.row().padTop(30f);
+        settingMenu.add(contentTable).expandX().expandY().padLeft(50);
+        settingMenu.row().padTop(30f);
+        settingMenu.add(bottomRightTable).expandX().right().padLeft(100); // Bottom-right table
+
+        // Center the menu on the screen
+        settingMenu.setPosition(
+                (screenWidth - settingMenu.getWidth()) / 2,
+                (screenHeight - settingMenu.getHeight()) / 2
+        );
+
+        stage.addActor(settingMenu);
+
+        closeButton.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        // Toggle the visibility of the small menu
+                        settingMenu.setVisible(!settingMenu.isVisible());
+                        table.setTouchable(Touchable.enabled);
+                    }
+                });
+
+        // Add event listener for the "Apply" button
+        applyButton.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Apply button clicked");
+                        settingsMenuDisplay.applyChanges(); // Apply the settings when clicked
+                        settingMenu.setVisible(false); // Optionally hide the settings menu
+                        table.setTouchable(Touchable.enabled);
+                    }
+                });
+    }
     private void addButtonElevationEffect(TextButton button) {
         button.addListener(new ClickListener() {
             @Override
