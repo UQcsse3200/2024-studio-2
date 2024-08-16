396ba72a765a50875138a0d395d070da40dcc1a2
ChristianMario4 <almario900@gmail.com>
2024-08-16 20:51:52 +1000

    Initialised basic class constructor and class description

--- source/core/src/main/com/csse3200/game/dialogue/DialogBox.java
+++ source/core/src/main/com/csse3200/game/dialogue/DialogBox.java
@@ -1,4 +1,18 @@
 package com.csse3200.game.dialogue;
 
-public class DialogBox {
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+/**
+ * Class used to represent the DialogBox displayed on screen
+ *
+ * The DialogBox will be called upon certain events triggers with NPCs and other mobs,
+ * illustrating information for game players to learn
+ *
+ * @author GPA Reapers (Team 11 UI(Stats))
+ */
+public class DialogBox extends Dialog {
+    public DialogBox(String title, Skin skin) {
+        super(title, skin);
+    }
 }
