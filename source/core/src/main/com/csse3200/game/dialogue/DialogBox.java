5ee8becc8603b247385ce1852cab785c95c5b7c7
ChristianMario4 <almario900@gmail.com>
2024-08-17 11:28:42 +1000

    Initialised some base methods and constructor for DialogBox class

--- source/core/src/main/com/csse3200/game/dialogue/DialogBox.java
+++ source/core/src/main/com/csse3200/game/dialogue/DialogBox.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.dialogue;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
@@ -12,7 +13,28 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
  * @author GPA Reapers (Team 11 UI(Stats))
  */
 public class DialogBox extends Dialog {
+    private Skin skin;
     public DialogBox(String title, Skin skin) {
         super(title, skin);
+        this.skin = skin;
     }
+
+    /**
+     * Adds the dialog box entity to the stage to be rendered
+     *
+     * @param stage - The stage on which the dialog box will be rendered onto
+     */
+    public void showDialogBox(Stage stage) {
+
+    }
+
+    /**
+     * Inputs the string to be outputted into the dialog box into the dialog class
+     *
+     * @param dialog - The string to be outputted into the dialog box
+     */
+    public void addText(String dialog) {
+
+    }
+
 }
