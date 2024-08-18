be0c360f80a61dc3c9f81cc6ca42c3d2ebbadc3b
Phong Nguyen <106220789+PHONGNGYUEN1@users.noreply.github.com>
2024-08-18 13:48:45 +1000

    Created Apple class and variables #3 #28

--- source/core/src/main/com/csse3200/game/inventory/items/food/Apple.java
+++ source/core/src/main/com/csse3200/game/inventory/items/food/Apple.java
@@ -1,4 +1,19 @@
 package com.csse3200.game.inventory.items.food;
 
-public class Apple {
+import com.badlogic.gdx.graphics.Texture;
+import com.csse3200.game.inventory.items.effects.feeding.FeedEffect;
+
+/**
+ * TO DO
+ */
+public class Apple extends AbstractFood {
+    protected Texture appleTexture;
+
+    /**
+     * Apple constructor
+     */
+    public Apple(Texture foodTexture) {
+        super("Apple", 5, 10, 5, new FeedEffect(2));
+        this.appleTexture = new Texture("food/foodTexture/apple.png");
+    }
 }
