c9d74c24895c278f61d975a6d9e7bf97c8b2fb16
Tarushi <tarushigera@gmail.com>
2024-08-18 20:27:57 +1000

    Removed a useless import

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -25,8 +25,6 @@ import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.border.Border;
-
 /**
  * A UI component for displaying the Main menu.
  */
