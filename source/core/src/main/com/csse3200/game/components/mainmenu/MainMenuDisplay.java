6512a128bd7b04d8e6d2e9ac44f80d00babec3a0
ishu <ishusharma20032008@gmail.com>
2024-08-21 10:12:50 +0530

    added help button

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;g
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -51,6 +51,7 @@ public class MainMenuDisplay extends UIComponent {
         TextButton loadBtn = new TextButton("Load", skin);
         TextButton minigamesBtn = new TextButton("Minigames", skin); // New Minigames button
         TextButton settingsBtn = new TextButton("Settings", skin);
+        TextButton helpBtn = new TextButton("Help", skin);
         TextButton exitBtn = new TextButton("Exit", skin);
         Label versionLabel = new Label("Version 1.0", skin);
 
@@ -58,6 +59,7 @@ public class MainMenuDisplay extends UIComponent {
         addButtonElevationEffect(loadBtn);
         addButtonElevationEffect(minigamesBtn); // Apply the elevation effect to Minigames button
         addButtonElevationEffect(settingsBtn);
+        addButtonElevationEffect(helpBtn);
         addButtonElevationEffect(exitBtn);
 
         startBtn.addListener(new ChangeListener() {
@@ -93,6 +95,15 @@ public class MainMenuDisplay extends UIComponent {
             }
         });
 
+        helpBtn.addListener(new ChangeListener() {
+            @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                logger.debug("Help button clicked");
+                entity.getEvents().trigger("help");
+                showHelpDialog();
+            }
+        });
+
         addExitConfirmation(exitBtn);
 
         table.add(title).padTop(50f).padBottom(50f);
@@ -105,6 +116,8 @@ public class MainMenuDisplay extends UIComponent {
         table.row();
         table.add(settingsBtn).padTop(15f).width(200f).height(60f);
         table.row();
+        table.add(helpBtn).padTop(15f).width(200f).height(60f);
+        table.row();
         table.add(exitBtn).padTop(15f).width(200f).height(60f);
         table.row();
         table.add(versionLabel).padTop(20f);
@@ -115,7 +128,113 @@ public class MainMenuDisplay extends UIComponent {
         addMinimizeButton();
 
         makeSettingMenu(settingMenu);
-    }
+        }
+        //method for help window
+        private void showHelpDialog() {
+            final int NUM_SLIDES = 6;
+            final float DIALOG_WIDTH = 1100f;
+            final float DIALOG_HEIGHT = 700f;
+
+            // Create the dialog with a larger size
+            final Dialog helpDialog = new Dialog("Help", skin) {
+                @Override
+                public void result(Object obj) {
+                    // Optional: handle result if needed
+                }
+            };
+
+            // Set the dialog size
+            helpDialog.setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
+
+            // Create a table to hold all slides
+            final Table slideTable = new Table();
+            slideTable.setFillParent(true);
+
+            Table[] slides = new Table[NUM_SLIDES];
+            for (int i = 0; i < NUM_SLIDES; i++) {
+                slides[i] = new Table();
+                Label titleLabel = new Label("Title " + (i + 1), skin, "title");
+                Label contentLabel = new Label("Content for slide " + (i + 1), skin);
+
+                slides[i].add(titleLabel).padTop(20f).expandX().center().row();
+                slides[i].add(contentLabel).padTop(20f).expandX().center().row();
+
+                // Add an exit button on the last slide
+                if (i == NUM_SLIDES - 1) {
+                    TextButton exitButton = new TextButton("Exit to Main Menu", skin);
+                    exitButton.addListener(new ChangeListener() {
+                        @Override
+                        public void changed(ChangeEvent event, Actor actor) {
+                            helpDialog.hide(); // Hide the dialog
+                            entity.getEvents().trigger("showMainMenu"); // Trigger the event to show the main menu
+                        }
+                    });
+
+                    slides[i].add(exitButton).padTop(30f).expandX().center();
+                }
+            }
+
+            // Add the first slide to the slideTable
+            slideTable.add(slides[0]).expand().fill();
+            helpDialog.add(slideTable).expand().fill();
+
+            // Create a table for navigation buttons
+            Table navigationTable = new Table();
+            TextButton previousButton = new TextButton("Previous", skin);
+            TextButton nextButton = new TextButton("Next", skin);
+
+            navigationTable.add(previousButton).padRight(10);
+            navigationTable.add(nextButton);
+
+            // Add the navigation buttons to the dialog's content
+            helpDialog.add(navigationTable).padTop(10).expandX().bottom().right();
+
+            final int[] currentSlide = {0}; // Use an array to modify value in lambda expressions
+
+            previousButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    if (currentSlide[0] > 0) {
+                        slides[currentSlide[0]].setVisible(false);
+                        currentSlide[0]--;
+                        slides[currentSlide[0]].setVisible(true);
+                    }
+                }
+            });
+
+            nextButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    if (currentSlide[0] < NUM_SLIDES - 1) {
+                        slides[currentSlide[0]].setVisible(false);
+                        currentSlide[0]++;
+                        slides[currentSlide[0]].setVisible(true);
+                    }
+                }
+            });
+
+            // Initially hide all slides except the first
+            for (int i = 1; i < NUM_SLIDES; i++) {
+                slides[i].setVisible(false);
+            }
+
+            // Center the dialog on the stage
+            helpDialog.setPosition(
+                    (stage.getWidth() - helpDialog.getWidth()) / 2,
+                    (stage.getHeight() - helpDialog.getHeight()) / 2
+            );
+
+            // Show the dialog
+            helpDialog.show(stage);
+
+            // Force layout update
+            helpDialog.layout();
+        }
+
+
+
+
+
 
     private void addMinimizeButton() {
         TextButton minimizeBtn = new TextButton("-", skin);
