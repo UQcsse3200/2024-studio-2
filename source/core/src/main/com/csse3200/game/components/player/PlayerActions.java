f908083db71d13398636cf027e33ba4707f14fdf
praneetdhoolia <praneet.dhoolia@gmail.com>
2024-08-23 04:51:08 +1000

    Integrated chunking logic into terrainfactory. Modified player speed and removed map bounds

--- source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
@@ -12,7 +12,7 @@ import com.csse3200.game.services.ServiceLocator;
  * and when triggered should call methods within this class.
  */
 public class PlayerActions extends Component {
-  private static final Vector2 MAX_SPEED = new Vector2(3f, 3f); // Metres per second
+  private static final Vector2 MAX_SPEED = new Vector2(3f*5, 3f*5); // Metres per second
 
   private PhysicsComponent physicsComponent;
   private Vector2 walkDirection = Vector2.Zero.cpy();
