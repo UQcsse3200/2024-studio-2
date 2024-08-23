98653d932d9e638ce50974ef0635fdcf62000008
theboah <noahboah234@hotmail.com>
2024-08-23 16:23:14 +1000

    Added conditional display of quests

--- source/core/src/main/com/csse3200/game/Overlays/QuestDisplay.java
+++ source/core/src/main/com/csse3200/game/Overlays/QuestDisplay.java
@@ -6,11 +6,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.csse3200.game.components.quests.AbstractQuest;
+import com.csse3200.game.components.quests.QuestManager;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.eventservice.EventService;
 import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Settings menu display and logic. If you bork the settings, they can be changed manually in
@@ -19,8 +24,27 @@ import org.slf4j.LoggerFactory;
 public class QuestDisplay extends UIComponent {
     private static final Logger logger = LoggerFactory.getLogger(QuestDisplay.class);
     EventService eventService = ServiceLocator.getEventService();
-
+    private Table background;
     private Table rootTable;
+
+    /** Comparator to sort quests showing active, completed then failed quests */
+    private final Comparator<AbstractQuest> questComparator = (q1, q2) -> {
+        if (q1.isActive() && !q2.isActive()) {
+            return -1;
+        } else if (!q1.isActive() && q2.isActive()) {
+            return 1;
+        } else if (q1.isQuestCompleted() && !q2.isQuestCompleted()) {
+            return -1;
+        } else if (!q1.isQuestCompleted() && q2.isQuestCompleted()) {
+            return 1;
+        } else if (q1.isFailed() && !q2.isFailed()) {
+            return -1;
+        } else if (!q1.isFailed() && q2.isFailed()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    };
 
     public QuestDisplay() {
         super();
@@ -37,19 +61,24 @@ public class QuestDisplay extends UIComponent {
         Image questsBackGround = new Image(
                 ServiceLocator.getResourceService()
                         .getAsset("images/QuestsOverlay/QuestsBG.png", Texture.class));
-
-// Create tables for buttons and quests
+        background = new Table();
+        background.setFillParent(true);
+        background.setDebug(true);
+        background.add(questsBackGround).center();
+        stage.addActor(background);
+        // Create tables for buttons and quests
         Table menuBtns = makeMenuBtns();
         Table quests = makeSliders();
-
         rootTable = new Table();
+        rootTable.setSize(background.getWidth(),background.getHeight());
         rootTable.setFillParent(true);
-        rootTable.add(questsBackGround).center();
+        //rootTable.setDebug(true);
+        rootTable.add(title).center();
+        if (quests.hasChildren()) {
+            rootTable.row();
+            rootTable.add(quests).padBottom(560f-quests.getRows()*40f);
+        }
         rootTable.row();
-        rootTable.add(title).center().padTop(-questsBackGround.getHeight()*1.1f);
-        rootTable.row().padTop(10f);
-        rootTable.add(quests).center();
-        rootTable.row().padTop(10f);
         rootTable.add(menuBtns).center();
 
         // Add the root table to the stage
@@ -57,30 +86,62 @@ public class QuestDisplay extends UIComponent {
     }
 
     private Table makeSliders() {
+        QuestManager questManager = (QuestManager) ServiceLocator.getEntityService().getSpecificComponent(QuestManager.class);
         Table table = new Table();
-        ProgressBar progressBarTest = new ProgressBar(0,2,1,false,skin);
-        Label titleTest = new Label("Test Quest", skin,"title", Color.BLACK);
-        table.add(titleTest);
-        table.add(progressBarTest).padRight(titleTest.getWidth());
+        if (questManager != null) {
+            List<AbstractQuest> questList = questManager.getAllQuests();
+
+            // Sort questList to display active, then completed, then failed quests.
+            questList.sort(questComparator);
+
+            for (AbstractQuest quest : questList) {
+                // Exclude Achievements and secret quests
+                if (!quest.isAchievement() && !quest.isSecret()) {
+
+                    // Change Display elements based on if quest is completed or failed
+                    Color questShownActive = Color.BROWN;
+                    if (quest.isQuestCompleted()) {
+                        questShownActive = Color.GOLD;
+                    }
+                    else if (quest.isFailed()) {
+                        questShownActive = Color.RED;
+                    }
+                    ProgressBar questProgressBar = new ProgressBar(0, quest.getNumQuestTasks(), 1, false, skin);
+                    questProgressBar.setValue(quest.getProgression());
+
+                    // Eventually will be ImageButton / TextButton that leads to more info on the quest
+                    Label questTitle = new Label(quest.getQuestName(), skin, "title", questShownActive);
+
+                    table.add(questTitle);
+                    table.add(questProgressBar).spaceLeft(20f);
+                    table.row().padTop(10f);
+                }
+            }
+        }
         return table;
     }
 
     private Table makeMenuBtns() {
         // Create buttons
-        TextButton exitBtn = new TextButton("Exit", skin);
+        TextButton exitBtn = new TextButton("Leave Menu", skin);
+
+        // Placeholder nextPage and previous Page button will be ImageButton as well
+        TextButton nextPage = new TextButton("Next Page", skin);
+        TextButton prevPage = new TextButton("Prev Page", skin);
 
         // Add listeners for button
         exitBtn.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                logger.debug("Exit button clicked");
                 exitMenu();
             }
         });
 
         // Layout buttons in a table
         Table table = new Table();
-        table.add(exitBtn).expandX().left().pad(15f, 0f, 0f, 15f);
+        table.add(prevPage).expandX().left().padRight(20f);
+        table.add(exitBtn).expandX().center();
+        table.add(nextPage).expandX().right().padLeft(20f);
 
         return table;
     }
@@ -102,6 +163,7 @@ public class QuestDisplay extends UIComponent {
 
     @Override
     public void dispose() {
+        background.clear();
         rootTable.clear();
         super.dispose();
     }
