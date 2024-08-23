4cf9a6473e328f0b88cb126f1ff874a031eb4d67
shalomi <s.panaduraarachchige@student.uq.edu.au>
2024-08-24 00:09:43 +1000

    created placeholder item for testing #3 #95

---
+++ source/core/src/main/com/csse3200/game/inventory/items/TestItem.java
@@ -0,0 +1,8 @@
+package com.csse3200.game.inventory.items;
+
+public class TestItem extends ConsumableItem {
+
+    public TestItem(String name, int itemCode, int limit, int quantity, String description) {
+        super(name, itemCode, limit, quantity, description);
+    }
+}
