565962e5fd25de286a6071d47affeab6510d40a3
mitsudz <MittunS2003@gmail.com>
2024-08-25 19:40:35 +1000

    Remove unused imports

--- source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
@@ -1,46 +1,15 @@
 package com.csse3200.game.entities.factories;
 
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.csse3200.game.areas.terrain.TerrainComponent;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.ai.tasks.AITaskComponent;
-import com.csse3200.game.components.CombatStatsComponent;
-import com.csse3200.game.components.npc.GhostAnimationController;
-import com.csse3200.game.components.npc.FriendlyNPCAnimationController;
-import com.csse3200.game.components.TouchAttackComponent;
-import com.csse3200.game.components.player.PlayerInventoryDisplay;
 import com.csse3200.game.components.tasks.*;
-import com.csse3200.game.components.ConfigComponent;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.entities.configs.*;
-import com.csse3200.game.files.FileLoader;
 import com.csse3200.game.inventory.items.AbstractItem;
 import com.csse3200.game.inventory.items.food.Foods;
 import com.csse3200.game.inventory.items.potions.healingpotion.HealingPotion;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
-import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
-import com.csse3200.game.physics.components.PhysicsMovementComponent;
-import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
-import com.csse3200.game.services.ServiceLocator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import java.util.List;
-import java.util.ArrayList;
-
-
 import static com.csse3200.game.physics.PhysicsLayer.OBSTACLE;
 
 
