d5287794ea7bb61e5b3b318b334995796f7b9f63
Ella Berglas <ella@berglas.org>
2024-08-26 13:06:25 +1000

    Added function comments

--- source/core/src/main/com/csse3200/game/screens/MiniGameMenuScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MiniGameMenuScreen.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
