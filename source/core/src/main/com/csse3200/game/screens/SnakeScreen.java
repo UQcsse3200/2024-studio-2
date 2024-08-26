c73894bf6c2d9102d67e0886b936e3449be03096
noahxl234 <noah@xelleron.com>
2024-08-26 21:40:44 +1000

    Corrected minigames exit with newscreen branch changes

--- source/core/src/main/com/csse3200/game/screens/SnakeScreen.java
+++ source/core/src/main/com/csse3200/game/screens/SnakeScreen.java
@@ -2,6 +2,7 @@ package com.csse3200.game.screens;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.services.eventservice.EventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.badlogic.gdx.Gdx;
@@ -71,6 +72,7 @@ public class SnakeScreen extends ScreenAdapter {
         ServiceLocator.registerEntityService(new EntityService());
         ServiceLocator.registerRenderService(new RenderService());
         ServiceLocator.registerTimeSource(new GameTime());
+        ServiceLocator.registerEventService(new EventService());
 
         renderer = RenderFactory.createRenderer();
         spriteBatch = new SpriteBatch();
