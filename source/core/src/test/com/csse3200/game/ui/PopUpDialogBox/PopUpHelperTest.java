8cd973bd11a80e861a0efe7fb0841066b70efc86
ChristianMario4 <almario900@gmail.com>
2024-08-27 21:42:22 +1000

    Added initial test files for PopUpHelper and PopupDialogBox

---
+++ source/core/src/test/com/csse3200/game/ui/PopUpDialogBox/PopUpHelperTest.java
@@ -0,0 +1,17 @@
+package com.csse3200.game.ui.PopUpDialogBox;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PopUpHelperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void displayDialog() {
+    }
+}
