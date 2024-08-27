2956d6c3c2f8b6164132cd9ec1d6cbf42c4d1d36
reemmi <reemzheem.hossain@student.uq.edu.au>
2024-08-27 19:07:29 +1000

    Tests added for QuestManager (excluding dialogue).

--- source/core/src/test/com/csse3200/game/components/quests/QuestManagerTest.java
+++ source/core/src/test/com/csse3200/game/components/quests/QuestManagerTest.java
@@ -1,30 +1,107 @@
 package com.csse3200.game.components.quests;
 
-
+import com.badlogic.gdx.audio.Sound;
+import com.csse3200.game.events.EventHandler;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.services.eventservice.EventService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(GameExtension.class)
 class QuestManagerTest {
-
     private QuestManager questManager;
+    private EventService eventService;
+    private EventHandler eventHandler;
+    private ResourceService resourceService;
 
     @BeforeEach
     void setUp() {
+        eventService = mock(EventService.class);
+        eventHandler = mock(EventHandler.class);
+        resourceService = mock(ResourceService.class);
+
+        Sound mockSound = mock(Sound.class);
+        when(resourceService.getAsset("sounds/QuestComplete.wav", Sound.class)).thenReturn(mockSound);
+
+
+        when(eventService.getGlobalEventHandler()).thenReturn(eventHandler);
+        ServiceLocator.registerEventService(eventService);
+        ServiceLocator.registerResourceService(resourceService);
+
         questManager = new QuestManager();
-        Task stepsTask = new Task("steps", "Take your first steps", "Just start moving!", 1);
-        //add tasks
-        List<Task> tasks = List.of(stepsTask);
-        QuestBasic quest = new QuestBasic("First Steps", "Take your first steps in this world!", tasks, false, false, null,null);
-        questManager.addQuest(quest);
     }
 
+    @Test
+    void AddQuest() {
+        QuestBasic quest = new QuestBasic("Test Quest", "Test Description", List.of(), false, false, null, null);
+        questManager.addQuest(quest);
+
+        assertEquals(quest, questManager.getQuest("Test Quest"));
+    }
+
+    @Test
+    void GetAllQuests() {
+        QuestBasic quest1 = new QuestBasic("Quest 1", "Description 1", List.of(), false, false, null, null);
+        QuestBasic quest2 = new QuestBasic("Quest 2", "Description 2", List.of(), false, false, null, null);
+        questManager.addQuest(quest1);
+        questManager.addQuest(quest2);
+
+        List<QuestBasic> quests = questManager.getAllQuests();
+        assertTrue(quests.contains(quest1));
+        assertTrue(quests.contains(quest2));
+    }
+
+    @Test
+    void HandleProgressQuest() {
+        Task task = new Task("testTask", "Test Task", "Description", 1);
+        QuestBasic quest = new QuestBasic("Test Quest", "Description", List.of(task), false, false, null, null);
+        questManager.addQuest(quest);
+
+        questManager.progressQuest("Test Quest", "testTask");
+        assertTrue(quest.isQuestCompleted());
+    }
+
+    @Test
+    void HandleQuestCompletion() {
+        Task task = new Task("testTask", "Test Task", "Description", 1);
+        QuestBasic quest = new QuestBasic("Test Quest", "Description", List.of(task), false, false, null, null);
+        questManager.addQuest(quest);
 
 
+        questManager.progressQuest("Test Quest", "testTask");
+        verify(eventHandler).trigger("questCompleted");
+        verify(eventHandler).trigger("Test Quest");
+    }
+
+    @Test
+    void HandleFailQuest() {
+        Task task = new Task("testTask", "Test Task", "Description", 1);
+        QuestBasic quest = new QuestBasic("Test Quest", "Description", List.of(task), false, false, null, null);
+        questManager.addQuest(quest);
+
+        questManager.failQuest("Test Quest");
+        assertTrue(quest.isFailed());
+    }
+
+    //@Test
+    //void shouldGetDialogueForCow() {
+
+   // }
 
 
+    @Test
+    void HandleNoQuestForProgression() {
 
-
+        questManager.progressQuest("Nonexistent Quest", "taskName");
+        verify(eventHandler, never()).trigger(anyString());
+    }
 }
