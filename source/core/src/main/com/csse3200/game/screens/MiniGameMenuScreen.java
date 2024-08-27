fe02776dc1c0b2809f1fbcfe13fa41140feb2b0c
24msingh24 <manandeep1500@gmail.com>
2024-08-27 23:32:21 +1000

    Changed the background of Minigamemenuscreen

--- source/core/src/main/com/csse3200/game/screens/MiniGameMenuScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MiniGameMenuScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -23,10 +24,14 @@ public class MiniGameMenuScreen implements Screen {
     private BitmapFont font;
     private final GdxGame game;
     private Skin skin;
+    private SpriteBatch batch;
 
     private Texture snakeTexture;
     private Texture skyTexture;
     private Texture waterTexture;
+
+    // New: Background Texture
+    private Texture backgroundTexture;
 
     public MiniGameMenuScreen(GdxGame game) {
         this.game = game;
@@ -37,16 +42,19 @@ public class MiniGameMenuScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        batch = new SpriteBatch();  // Create SpriteBatch for rendering
         font = new BitmapFont();
-
         font.getData().setScale(2);
 
         skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
 
-        TextButton exitButton = new TextButton("Exit",skin);
+        // Load Textures
+        backgroundTexture = new Texture(Gdx.files.internal("/Users/manandeepsingh/Desktop/Studio-2_2024/2024-studio-2/source/core/assets/images/BackgroundSplash.png")); // Path to your background image
         snakeTexture = new Texture(Gdx.files.internal("images/minigames/Snake.png"));
         skyTexture = new Texture(Gdx.files.internal("images/minigames/Flappy_bird.png"));
         waterTexture = new Texture(Gdx.files.internal("images/minigames/Underwater_maze.png"));
+
+        TextButton exitButton = new TextButton("Exit", skin);
 
         Image snakeImage = new Image(snakeTexture);
         Image skyImage = new Image(skyTexture);
@@ -60,27 +68,42 @@ public class MiniGameMenuScreen implements Screen {
         table.setFillParent(true);
         table.center();
 
-        table.add(snakeImage).pad(10).row();
-        table.add(snakeButton).pad(10).row();
-        table.add(skyImage).pad(10).row();
-        table.add(skyButton).pad(10).row();
-        table.add(waterImage).pad(10).row();
-        table.add(waterButton).pad(10).row();
+// Create a cell for the snake image and button
+        Table snakeTable = new Table();
+        snakeTable.add(snakeImage).padBottom(10).row();
+        snakeTable.add(snakeButton).padBottom(10);
 
+// Create a cell for the sky image and button
+        Table skyTable = new Table();
+        skyTable.add(skyImage).padBottom(10).row();
+        skyTable.add(skyButton).padBottom(10);
+
+// Create a cell for the water image and button
+        Table waterTable = new Table();
+        waterTable.add(waterImage).padBottom(10).row();
+        waterTable.add(waterButton).padBottom(10);
+
+// Add all the image/button pairs to the main table in a single row
+        table.add(snakeTable).pad(20);
+        table.add(skyTable).pad(20);
+        table.add(waterTable).pad(20);
+
+// Add the table to the stage
         stage.addActor(table);
+
+
+
 
         Table exitButtonTable = new Table();
         exitButtonTable.setFillParent(true);
         exitButtonTable.top().right();
 
         exitButtonTable.add(exitButton).pad(10);
-
         stage.addActor(exitButtonTable);
 
         exitButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Return to main screen
                 game.setScreen(new MainMenuScreen(game));
             }
         });
@@ -88,56 +111,43 @@ public class MiniGameMenuScreen implements Screen {
         snakeImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 snakeImage.setColor(Color.GREEN);
-                // Switch to SnakeScreen
                 game.setScreen(new SnakeScreen(game));
             }
         });
 
-        // TODO: Will update as new minigames are implemented.
         skyImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 skyImage.setColor(Color.GREEN);
             }
         });
 
-        // TODO: Will update as new minigames are implemented.
         waterImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 waterImage.setColor(Color.GREEN);
             }
         });
 
-
         snakeButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 snakeButton.setColor(Color.GREEN);
-                // Switch to SnakeScreen
                 game.setScreen(new SnakeScreen(game));
             }
         });
 
-        // TODO: Will update as new minigames are implemented.
         skyButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 skyButton.setColor(Color.GREEN);
             }
         });
 
-        // TODO: Will update as new minigames are implemented.
         waterButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Change button color to green when clicked
                 waterButton.setColor(Color.GREEN);
             }
         });
@@ -145,16 +155,19 @@ public class MiniGameMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Set the background color to black
+        // Set background color
         Gdx.gl.glClearColor(248f / 255f, 249f / 255f, 178f / 255f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Draw background image
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // Draws background
+        batch.end();
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
 
-        // enable escape key functionality back to Main Menu
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {  // Go to minigames menu
-            Gdx.gl.glClearColor(248f / 255f, 249f / 255f, 178f / 255f, 1f);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new MainMenuScreen(game));
         }
     }
@@ -166,12 +179,10 @@ public class MiniGameMenuScreen implements Screen {
 
     @Override
     public void pause() {
-        // Handle pause
     }
 
     @Override
     public void resume() {
-        // Handle resume
     }
 
     @Override
@@ -183,12 +194,11 @@ public class MiniGameMenuScreen implements Screen {
     public void dispose() {
         stage.dispose();
         font.dispose();
-        skin.dispose(); // Dispose of the skin when done
-        snakeTexture.dispose(); // Dispose of the textures
+        skin.dispose();
+        snakeTexture.dispose();
         skyTexture.dispose();
         waterTexture.dispose();
-
-        // Added to make sure the game screen colour stays the same at the end
-        Gdx.gl.glClearColor(248f / 255f, 249f / 255f, 178f / 255f, 1f);
+        backgroundTexture.dispose();  // Dispose of the background texture
+        batch.dispose();  // Dispose of the SpriteBatch
     }
 }
