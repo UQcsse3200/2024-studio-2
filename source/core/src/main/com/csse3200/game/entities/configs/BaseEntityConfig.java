732c9e15a160d6fb383c473c4ecd637fcd5f62ea
Bailey Stoodley <Bailey@bunnycup.com>
2024-08-28 07:59:22 +1000

    Adding docs to BaseEntityConfig

--- source/core/src/main/com/csse3200/game/entities/configs/BaseEntityConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/BaseEntityConfig.java
@@ -19,30 +19,65 @@ public class BaseEntityConfig {
 
     protected BaseEntityConfig() {}
 
+    /**
+     * Gets the path to the sprite image used for rendering the entity.
+     *
+     * @return the sprite path as a String.
+     */
     public String getSpritePath() {
         return this.spritePath;
     }
 
+    /**
+     * Gets the speed at which the entity's animation plays.
+     *
+     * @return the animation speed as a float.
+     */
     public float getAnimationSpeed() {
         return this.animationSpeed;
     }
 
+    /**
+     * Gets the array of file paths to sound effects associated with the entity.
+     *
+     * @return an array of sound paths as String[].
+     */
     public String[] getSoundPath() {
         return this.soundPath;
     }
 
+    /**
+     * Gets the health of the entity.
+     *
+     * @return the health as an int.
+     */
     public int getHealth() {
         return this.health;
     }
 
+    /**
+     * Gets the name of the animal or entity.
+     *
+     * @return the animal name as a String.
+     */
     public String getAnimalName() {
         return animalName;
     }
 
+    /**
+     * Gets the base attack value of the entity.
+     *
+     * @return the base attack value as an int.
+     */
     public int getBaseAttack () {
         return this.baseAttack;
     }
 
+    /**
+     * Gets the array of hints related to the entity.
+     *
+     * @return an array of hints as String[].
+     */
     public String[] getBaseHint() {
         return baseHint;
     }
