38694ace9d40ba831079bffb615692efbc56b5b3
Bailey Stoodley <Bailey@bunnycup.com>
2024-08-27 18:33:30 +1000

    Fixing baseEntityConfig inits as per PR

--- source/core/src/main/com/csse3200/game/entities/configs/BaseEntityConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/BaseEntityConfig.java
@@ -6,11 +6,11 @@ package com.csse3200.game.entities.configs;
 public class BaseEntityConfig {
     public int health = 1; // Remain public as per the base game
     public int baseAttack = 0; // Remain public as per the base game
-    protected String animalName = "";
-    protected String[] baseHint = null;
-    protected String spritePath = "images/ghost.atlas";
+    protected String animalName;
+    protected String[] baseHint;
+    protected String spritePath;
     protected float animationSpeed = 0.1f;
-    protected String[] soundPath = null;
+    protected String[] soundPath;
     public int hunger = 100;
     public int strength = 0;
     public int defense = 0;
