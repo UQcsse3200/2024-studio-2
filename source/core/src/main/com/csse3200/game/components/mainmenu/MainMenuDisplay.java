de8d313b427a4c75d826d2ce849103c387f94407
Tarushi <tarushigera@gmail.com>
2024-08-28 15:33:06 +1000

    Fixed exit confirmation

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -108,17 +108,6 @@ public class MainMenuDisplay extends UIComponent {
             }
         });
 
-
-    exitBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-
-            logger.debug("Exit button clicked");
-            entity.getEvents().trigger("exit");
-          }
-        });
-
       minigamesBtn.addListener(
               new ChangeListener() {
                   @Override
