10741ac1421fc702fce7ce68c6d37fd2c13979f0
jaden-z-fng <jfang5768@gmail.com>
2024-08-28 12:14:22 +1000

    Added javadocs for new functions for achievements in QuestManager and new classes AchievementDisplay and AchievementsScreen. Added JUnit testing for QuestHidden.

--- source/core/src/main/com/csse3200/game/components/quests/QuestHidden.java
+++ source/core/src/main/com/csse3200/game/components/quests/QuestHidden.java
@@ -2,14 +2,8 @@ package com.csse3200.game.components.quests;
 
 
 public class QuestHidden extends AbstractQuest {
-    /**A description of the task. */
     private final String questName;
-
-    /**
-     * A description of the task.
-     */
     private final String questDescription;
-
     private boolean completed = false;
 
     public QuestHidden(String questName, String questDescription) {
@@ -17,25 +11,33 @@ public class QuestHidden extends AbstractQuest {
         this.questDescription = questDescription;
     }
 
+    /**
+     * Getter method for the quest name.
+     * @return String representation of the quest's name.
+     */
     public String getQuestName() {
         return questName;
     }
 
+    /**
+     * Getter method for the quest description.
+     * @return String representation of the quest's description
+     */
     public String getQuestDescription() {
         return questDescription;
     }
-    /** Returns the description of the current subtask being completed
-     * for a test or "QUEST COMPLETED" if the quest is completed.*/
 
     /**
-     * Returns the hint for the current subtask being completed
-     * for a test or "QUEST COMPLETED" if the quest is completed.
+     * Checks if the current achievement has been completed
+     * @return True if it has been completed, False otherwise
      */
-
     public boolean isCompleted() {
         return this.completed;
     }
 
+    /**
+     * Complete the current achievement.
+     */
     public void complete() {
         this.completed = true;
     }
