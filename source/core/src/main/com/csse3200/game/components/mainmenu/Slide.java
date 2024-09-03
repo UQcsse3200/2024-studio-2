46117e2c2b27c7019f98a77b8795593fe49c897e
mitsudz <MittunS2003@gmail.com>
2024-09-03 19:02:16 +1000

    Some changes, but things are going wrong

--- source/core/src/main/com/csse3200/game/components/mainmenu/Slide.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/Slide.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Slide extends Table {
     private final String name;
 
-    private Slide(String name, Skin skin, String title, String contents) {
+    public Slide(String name, Skin skin, String title, String contents) {
         this.name = name;
         Label titleLabel = new Label(title, skin, "title");
         Label contentLabel = new Label(contents, skin);
@@ -21,34 +21,4 @@ public class Slide extends Table {
     public String getName() {
         return this.name;
     }
-
-    public static List<Slide> getAllSlides(Skin skin) {
-        return loadSlidesFromJson(skin);
-    }
-
-    private static List<Slide> loadSlidesFromJson(Skin skin) {
-        List<Slide> slides = new ArrayList<>();
-
-        // Read array of SlideData from JSON
-        SlideData[] slidesData = FileLoader.readClass(SlideData[].class, "configs/HelpSlides.json");
-
-        if (slidesData != null) {
-            for (SlideData data : slidesData) {
-                Slide slide = new Slide(data.name, skin, data.title, data.content);
-                slides.add(slide);
-            }
-        } else {
-            throw new RuntimeException("Error: Slide data could not be loaded from slides.json.");
-        }
-
-        return slides;
-    }
-
-    // Helper class to represent slide data loaded from JSON
-    private static class SlideData {
-        public String name;
-        public String title;
-        public String content;
-    }
-
 }
