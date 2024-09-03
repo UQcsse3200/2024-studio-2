c852e940cd1dff1ee642f628a17e2a7b03976919
shaivikaaaa <shaivika.anand@gmail.com>
2024-09-04 01:59:01 +1000

    selection screen for the water kingdom

--- source/core/src/main/com/csse3200/game/screens/WaterAnimalSelectionScreen.java
+++ source/core/src/main/com/csse3200/game/screens/WaterAnimalSelectionScreen.java
@@ -1,2 +1,47 @@
-package com.csse3200.game.screens;public class WaterAnimalSelectionScreen {
+package com.csse3200.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.components.animal.WaterAnimalSelectionDisplay;
+import com.csse3200.game.components.animal.AnimalSelectionActions;
+import com.csse3200.game.ui.PopUpDialogBox.PopUpHelper;
+
+public class WaterAnimalSelectionScreen extends ScreenAdapter {
+    private Stage stage;
+    private WaterAnimalSelectionDisplay display;
+    private AnimalSelectionActions actions;
+
+    public WaterAnimalSelectionScreen(GdxGame game) {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        Skin skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
+        PopUpHelper dialogHelper = new PopUpHelper(skin, stage);
+
+        display = new WaterAnimalSelectionDisplay(stage, skin);
+        actions = new AnimalSelectionActions(display, dialogHelper, game);
+
+        actions.resetSelection();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
 }
