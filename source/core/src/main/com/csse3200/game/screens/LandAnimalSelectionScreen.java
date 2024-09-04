3a45f97443ca9abf1060868a51b0d641de1e9b27
shaivikaaaa <shaivika.anand@gmail.com>
2024-09-04 16:19:22 +1000

    added a new button which opens to the water kingdom selection screen

--- source/core/src/main/com/csse3200/game/screens/LandAnimalSelectionScreen.java
+++ source/core/src/main/com/csse3200/game/screens/LandAnimalSelectionScreen.java
@@ -3,8 +3,11 @@ package com.csse3200.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.components.animal.LandAnimalSelectionDisplay;
 import com.csse3200.game.components.animal.AnimalSelectionActions;
@@ -15,6 +18,7 @@ public class LandAnimalSelectionScreen extends ScreenAdapter {
     private LandAnimalSelectionDisplay display;
     private AnimalSelectionActions actions;
 
+    private GdxGame game;
 
     /**
      * Constructor for AnimalSelectionScreen.
@@ -24,7 +28,8 @@ public class LandAnimalSelectionScreen extends ScreenAdapter {
     public LandAnimalSelectionScreen(GdxGame game) {
         // Initialize the stage, which is the root container for all UI elements
         stage = new Stage();
-        Gdx.input.setInputProcessor(stage);  // Set the stage as the input processor to handle user input
+        Gdx.input.setInputProcessor(stage);  // Set -up of  stage as the input processor to handle user input
+        this.game = game;
 
         // Load the skin for UI elements from the specified JSON file
         Skin skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
@@ -37,6 +42,7 @@ public class LandAnimalSelectionScreen extends ScreenAdapter {
 
         // Set up actions for handling UI interactions, passing the display, dialog helper, and game instance
         actions = new AnimalSelectionActions(display, dialogHelper, game);
+        addButtonToSwitchScreen("Water Animals", WaterAnimalSelectionScreen.class, skin);
 
         actions.resetSelection(); // Reset the selection when the screen is created
     }
@@ -76,4 +82,22 @@ public class LandAnimalSelectionScreen extends ScreenAdapter {
         // Dispose of the stage to free up resources
         stage.dispose();
     }
+    private void addButtonToSwitchScreen(String buttonText, final Class<? extends ScreenAdapter> screenClass, Skin skin) {
+        TextButton button = new TextButton(buttonText, skin);
+        button.setPosition(50, stage.getHeight() - 100 * (stage.getActors().size + 1));  // Position buttons dynamically
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                try {
+                    // Switch to the new screen
+                    ScreenAdapter newScreen = screenClass.getConstructor(GdxGame.class).newInstance(game);
+                    game.setScreen(newScreen);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        stage.addActor(button);
+    }
+
 }
