40f5f472eb2bda1600d3c6a8e1259652c9e450e9
Owen Harding <owenrharding@gmail.com>
2024-09-07 09:52:57 +1000

    Deleted CombatArea and updated CombatManager structure/logic so it is move matchup dependent.

--- source/core/src/main/com/csse3200/game/components/combat/CombatManager.java
+++ source/core/src/main/com/csse3200/game/components/combat/CombatManager.java
@@ -6,7 +6,7 @@ import com.csse3200.game.components.CombatStatsComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * Manages the turn-based combat loop and handles attacks
@@ -14,13 +14,13 @@ import java.util.Objects;
 public class CombatManager extends Component {
     private static final Logger logger = LoggerFactory.getLogger(CombatManager.class);
 
-    private enum Turn { INIT, PLAYER, ENEMY }
-    private Turn currentTurn;
-    public enum Action { ATTACK, GUARD, COUNTER };
+    public enum Action { START_MOVE, ATTACK, GUARD, COUNTER };
     private final Entity player;
     private final Entity enemy;
     private final CombatStatsComponent playerStats;
     private final CombatStatsComponent enemyStats;
+    private Action playerAction;
+    private Action enemyAction;
     private boolean isCombatEnd = false;
 
     public CombatManager(Entity player, Entity enemy) {
@@ -30,70 +30,99 @@ public class CombatManager extends Component {
         this.playerStats = player.getComponent(CombatStatsComponent.class);
         this.enemyStats = enemy.getComponent(CombatStatsComponent.class);
 
-        this.currentTurn = Turn.INIT;
+        this.playerAction = Action.START_MOVE;
+        this.enemyAction = Action.START_MOVE;
     }
 
     /**
-     * Player has clicked attack button to select 'attack' as their action.
+     * Both opponents' actions need to be determined before either action is enacted, as the sequencing of the moves
+     * will be dependent on both of the choices.
+     * @param playerActionStr
      */
-    public void onAttackSelected()
+    public void onPlayerActionSelected(String playerActionStr)
     {
-        checkTurn();
+        playerAction = Action.valueOf(playerActionStr);
 
-        if (currentTurn == Turn.PLAYER) {
-            enemyStats.hit(playerStats);
-        } else {
-            playerStats.hit(enemyStats);
-        }
+        selectEnemyMove();
 
-        checkCombatEnd();
-
-        switchTurn();
-
-        if (currentTurn == Turn.PLAYER) {
-            enemyStats.hit(playerStats);
-        } else {
-            playerStats.hit(enemyStats);
-        }
-
-        checkCombatEnd();
+        processActions();
     }
 
-    /**
-     * Sets the current turn to whoever currently has the fastest speed stat.
-     */
-    private void checkTurn() {
-        if (playerStats.getSpeed() >= enemyStats.getSpeed()) {
-            this.currentTurn = Turn.PLAYER;
+    private Action selectEnemyMove()
+    {
+        Action enemyAction;
+
+        int rand = (int) (Math.random() * 3);
+        if (rand == 0) {
+            enemyAction = Action.ATTACK;
+        } else if (rand == 1) {
+            enemyAction = Action.GUARD;
         } else {
-            this.currentTurn = Turn.ENEMY;
+            enemyAction = Action.COUNTER;
+        }
+
+        return enemyAction;
+    }
+
+    private void processActions()
+    {
+        if (playerAction == Action.ATTACK)
+        {
+            if (enemyAction == Action.ATTACK)
+            {
+                // Fastest entity moves first.
+            }
+            else if (enemyAction == Action.GUARD)
+            {
+
+            }
+            else if (enemyAction == Action.COUNTER)
+            {
+
+            }
+        }
+        else if (playerAction == Action.GUARD)
+        {
+            if (enemyAction == Action.ATTACK)
+            {
+
+            }
+            else if (enemyAction == Action.GUARD)
+            {
+
+            }
+            else if (enemyAction == Action.COUNTER)
+            {
+
+            }
+        }
+        else if (playerAction == Action.COUNTER)
+        {
+            if (enemyAction == Action.ATTACK)
+            {
+
+            }
+            else if (enemyAction == Action.GUARD)
+            {
+
+            }
+            else if (enemyAction == Action.COUNTER)
+            {
+
+            }
         }
     }
 
-    /**
-     * Switches currentTurn to opposite of current currentTurn.
-     */
-    private void switchTurn() {
-        if (currentTurn == Turn.PLAYER) {
-            currentTurn = Turn.ENEMY;
-        } else {
-            currentTurn = Turn.PLAYER;
-        }
-    }
-
-    private void checkCombatEnd() {
+    private void checkCombatEnd()
+    {
         if (playerStats.getHealth() <= 0 || enemyStats.getHealth() <= 0) {
             isCombatEnd = true;
             logger.info("Combat ended");
         }
     }
 
-    public Entity getPlayer() {
-        return player;
-    }
-    public Entity getEnemy() {
-        return enemy;
-    }
+    public Entity getPlayer() { return player; }
+    public Entity getEnemy() { return enemy; }
 
     @Override
     public void render(float delta) {
