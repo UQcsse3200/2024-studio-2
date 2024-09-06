2e6fa488224e6f7ddb8d2b5bfaa7d4d1bc817b20
Bailey Stoodley <Bailey@bunnycup.com>
2024-09-06 14:53:25 +1000

    Added a AvoidTask so that the friendly NPCs avoid a certain specified entity. The changes are in AvoidTask.java.

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -95,7 +95,6 @@ public class ForestGameArea extends GameArea {
   public void displayUI() {
     Entity ui = new Entity();
     ui.addComponent(new GameAreaDisplay("Box Forest"));
-    ui.addComponent(new QuestPopup());
     spawnEntity(ui);
   }
 
