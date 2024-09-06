92a5b4604948dd057e739607a8865a217fe6a4ce
Bailey Stoodley <Bailey@bunnycup.com>
2024-09-06 16:09:36 +1000

    Fixing code smells

--- source/core/src/test/com/csse3200/game/components/tasks/PauseTaskTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/PauseTaskTest.java
@@ -2,7 +2,6 @@ package com.csse3200.game.components.tasks;
 
 import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.ai.tasks.AITaskComponent;
-import com.csse3200.game.components.quests.QuestManager;
 import com.csse3200.game.components.ConfigComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.NPCConfigs;
