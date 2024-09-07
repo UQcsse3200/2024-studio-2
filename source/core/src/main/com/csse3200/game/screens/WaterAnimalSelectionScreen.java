d883c405b96c8504f3dedea90c174479bc29050e
shaivikaaaa <shaivika.anand@gmail.com>
2024-09-08 01:47:40 +1000

    refactored for water animal screen

--- source/core/src/main/com/csse3200/game/screens/WaterAnimalSelectionScreen.java
+++ source/core/src/main/com/csse3200/game/screens/WaterAnimalSelectionScreen.java
@@ -1,47 +1,19 @@
 package com.csse3200.game.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.components.animal.WaterAnimalSelectionDisplay;
-import com.csse3200.game.components.animal.AnimalSelectionActions;
-import com.csse3200.game.ui.PopUpDialogBox.PopUpHelper;
+import com.csse3200.game.components.animal.AnimalSelectionDisplay;
 
-public class WaterAnimalSelectionScreen extends ScreenAdapter {
-    private Stage stage;
-    private WaterAnimalSelectionDisplay display;
-    private AnimalSelectionActions actions;
+public class WaterAnimalSelectionScreen extends AnimalSelectionScreen {
 
     public WaterAnimalSelectionScreen(GdxGame game) {
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-
-        Skin skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
-        PopUpHelper dialogHelper = new PopUpHelper(skin, stage);
-
-        display = new WaterAnimalSelectionDisplay(stage, skin);
-        actions = new AnimalSelectionActions(display, dialogHelper, game);
-
-        actions.resetSelection();
+        super(game);
     }
 
     @Override
-    public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
+    protected AnimalSelectionDisplay createDisplay(Stage stage, Skin skin) {
+        return new WaterAnimalSelectionDisplay(stage, skin);
     }
 }
