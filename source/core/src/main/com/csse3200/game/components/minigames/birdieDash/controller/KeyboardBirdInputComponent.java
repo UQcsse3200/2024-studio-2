d1abe8775964718c1f5b3fc6e232472183f12205
24msingh24 <manandeep1500@gmail.com>
2024-09-07 22:15:09 +1000

    Input functionality to Flappy Bird

---
+++ source/core/src/main/com/csse3200/game/components/minigames/birdieDash/controller/KeyboardBirdInputComponent.java
@@ -0,0 +1,30 @@
+package com.csse3200.game.components.minigames.birdieDash.controller;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.csse3200.game.components.minigames.Direction;
+import com.csse3200.game.input.InputComponent;
+
+
+public class KeyboardBirdInputComponent extends InputComponent {
+
+
+    public KeyboardBirdInputComponent() {
+        super(5);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(!this.enabled){
+            return false;
+        }
+        return switch (keycode) {
+            case Keys.Q -> {
+                entity.getEvents().trigger("flap");
+                yield true;
+            }
+
+            default -> false;
+        };
+    }
+}
