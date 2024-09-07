324c6f4a06fecf648b6aded757fc4295f3dcfe1f
ishu <ishusharma20032008@gmail.com>
2024-09-07 21:06:48 +0530

    movement control for help menu

--- source/core/src/main/com/csse3200/game/components/mainmenu/Slides.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/Slides.java
@@ -1,22 +1,97 @@
 package com.csse3200.game.components.mainmenu;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 
 public class Slides {
 
     public static class MovementSlide extends Table {
+        private DogActor dogActor;
+        private final float speed= 100f;
         public MovementSlide(Skin skin) {
             Label titleLabel = new Label("Movement Instructions", skin, "title");
-            Label contentLabel = new Label("Here are the instructions for movement...", skin);
+            Label contentLabel = new Label("Move the dog with WASD keys!!", skin);
 
             add(titleLabel).padTop(20f).expandX().center().row();
             add(contentLabel).padTop(20f).expandX().center().row();
+            // Create the dog actor and add it to the table
+            dogActor = new DogActor(new Texture(Gdx.files.internal("images/dog2.png")));
+            addActor(dogActor);  // Adds the dog to the stage
+        }
 
+
+        // Custom DogActor class to handle movement
+        public class DogActor extends Actor {
+            private Texture texture;
+            private Drawable drawable;
+            private float dogX, dogY;
+
+            public DogActor(Texture texture) {
+                this.texture = texture;
+                this.drawable = new TextureRegionDrawable(new TextureRegion(texture));
+                setSize(texture.getWidth(), texture.getHeight());
+
+                // Initially, the position will be set in the layout method
+            }
+
+            @Override
+            public void act(float delta) {
+                super.act(delta);
+
+                // Update dog position based on user input
+                handleInput(delta);
+
+                // Update the actor's position
+                setPosition(dogX, dogY);
+            }
+
+            @Override
+            public void draw(com.badlogic.gdx.graphics.g2d.Batch batch, float parentAlpha) {
+                drawable.draw(batch, getX(), getY(), getWidth(), getHeight());
+            }
+
+            private void handleInput(float delta) {
+                // Initialize position if it's the first time act is called
+                if (dogX == 0 && dogY == 0) {
+                    dogX = getX();
+                    dogY = getY();
+                }
+
+                // Handle WASD input
+                if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+                    dogY += speed * delta;
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+                    dogY -= speed * delta;
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+                    dogX -= speed * delta;
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+                    dogX += speed * delta;
+
+                }
+
+                // Boundary check to keep the dog inside the help menu (MovementSlide)
+                dogX = Math.max(0, Math.min(dogX, getParent().getWidth() - getWidth()));
+                dogY = Math.max(0, Math.min(dogY, getParent().getHeight() - getHeight()));
+            }
         }
     }
+
 
 
     /**
