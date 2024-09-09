0244455ca32d42d968d7b7bcba027965a3458200
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-09-09 15:18:05 +1000

    Add log for execute() in GuardMove

--- source/core/src/main/com/csse3200/game/components/combat/move/GuardMove.java
+++ source/core/src/main/com/csse3200/game/components/combat/move/GuardMove.java
@@ -1,15 +1,25 @@
 package com.csse3200.game.components.combat.move;
 
 import com.csse3200.game.components.CombatStatsComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GuardMove extends CombatMove {
+    private static final Logger logger = LoggerFactory.getLogger(AttackMove.class);
+
     public GuardMove(String moveName, int staminaCost) {
         super(moveName, staminaCost);
     }
 
     @Override
     public void execute(CombatStatsComponent attackerStats) {
-        attackerStats.addStamina(-(this.getStaminaCost()));
+        if (attackerStats != null) {
+            logger.info("Guard Move: {} using {} stamina.", this.getMoveName(), this.getStaminaCost());
+
+            attackerStats.addStamina(-(this.getStaminaCost()));
+        } else {
+            logger.error("Entity does not have CombatStatsComponent.");
+        }
     }
 
     @Override
