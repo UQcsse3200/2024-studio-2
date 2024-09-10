55f94ffe8441e6da2420ee3db1b91370332d9f30
kungkemi-admin <work.a.a.johnson@gmail.com>
2024-09-10 21:59:55 +1000

    Removes main-menu button

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
@@ -27,7 +27,6 @@ public class MainMenuActions extends Component {
     entity.getEvents().addListener("exit", this::onExit);
     entity.getEvents().addListener("achievements", this::onAchievements);
     entity.getEvents().addListener("SnakeGame", this::onSnakeMiniGame);
-    entity.getEvents().addListener("quickTimeEvent", this::onQuickTimeEvent);
   }
 
   /**
@@ -78,11 +77,6 @@ public class MainMenuActions extends Component {
   private void onSnakeMiniGame() {
     logger.info("Launching settings screen");
     game.setScreen(GdxGame.ScreenType.MINI_GAME_MENU_SCREEN);
-  }
-
-  private void onQuickTimeEvent() {
-    logger.info("Launching quick-time event screen");
-    game.setScreen(GdxGame.ScreenType.QUICK_TIME_EVENT);
   }
 
 
