c64edc998215c0bd34235427d686aefb98124721
mitsudz <MittunS2003@gmail.com>
2024-09-11 07:20:27 +1000

    Refactor

--- source/core/src/main/com/csse3200/game/gamestate/data/InventorySave.java
+++ source/core/src/main/com/csse3200/game/gamestate/data/InventorySave.java
@@ -2,7 +2,6 @@ package com.csse3200.game.gamestate.data;
 
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
-import com.csse3200.game.components.quests.QuestBasic;
 import com.csse3200.game.inventory.items.AbstractItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +9,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 
 public class InventorySave implements Json.Serializable {
+    private final Logger logger = LoggerFactory.getLogger(InventorySave.class);
     public AbstractItem[] inventoryContent = new AbstractItem[0];
-    private Logger logger = LoggerFactory.getLogger(InventorySave.class);
 
     public void write(Json json) {
         json.writeArrayStart("inventory");
