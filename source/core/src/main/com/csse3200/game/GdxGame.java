fe7acecc035658afd7f0202a6d4760df7983465d
Jose <jleoncio2810@gmail.com>
2024-09-11 14:11:29 +1000

    Replicated GameOverWin code as template for intro story as it is an existing screen that most resembles the desirable screen.

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -152,6 +152,8 @@ public class GdxGame extends Game {
               return new GameOverWinScreen(this);
           case GAME_OVER_LOSE:
               return new GameOverLoseScreen(this);
+          case STORY:
+              return new StoryScreen(this);
 
 
           default:
@@ -164,7 +166,7 @@ public class GdxGame extends Game {
    */
   public enum ScreenType {
 
-      MAIN_MENU, MAIN_GAME, MINI_GAME_MENU_SCREEN, LOADING_SCREEN, ANIMAL_SELECTION, ACHIEVEMENTS, COMBAT, BOSS_CUTSCENE, GAME_OVER_WIN, GAME_OVER_LOSE, SNAKE_MINI_GAME
+      MAIN_MENU, MAIN_GAME, MINI_GAME_MENU_SCREEN, LOADING_SCREEN, ANIMAL_SELECTION, ACHIEVEMENTS, COMBAT, BOSS_CUTSCENE, GAME_OVER_WIN, GAME_OVER_LOSE, SNAKE_MINI_GAME, STORY
 
   }
 
