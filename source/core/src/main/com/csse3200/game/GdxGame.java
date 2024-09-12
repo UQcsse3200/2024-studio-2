8e898f2465642321a8e5dd9fba2f81320fff371e
nownver <urawee.thani@gmail.com>
2024-09-12 11:31:27 +1000

    Adding Transition screen before combat

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -87,6 +87,10 @@ public class GdxGame extends Game {
         addScreen(ScreenType.BOSS_CUTSCENE, getScreen(), player, enemy);
     }
 
+    public void addEnemyCutsceneScreen(Entity player, Entity enemy) {
+        addScreen(ScreenType.ENEMY_CUTSCENE, getScreen(), player, enemy);
+    }
+
     public void enterCombatScreen(Entity player, Entity enemy) {
         addScreen(ScreenType.COMBAT, getScreen(), player, enemy);
     }
@@ -145,6 +149,8 @@ public class GdxGame extends Game {
                 return new CombatScreen(this, screen, container, player, enemy);
             case BOSS_CUTSCENE:
                 return new BossCutsceneScreen(this, screen, container, player, enemy);
+            case ENEMY_CUTSCENE:
+                return new EnemyCutsceneScreen(this, screen, container, player, enemy);
             case ACHIEVEMENTS:
                 return new AchievementsScreen(this);
             case MINI_GAME_MENU_SCREEN:
@@ -173,7 +179,7 @@ public class GdxGame extends Game {
      */
     public enum ScreenType {
         MAIN_MENU, MAIN_GAME, SETTINGS, MINI_GAME_MENU_SCREEN, LOADING_SCREEN, ANIMAL_SELECTION,
-        ACHIEVEMENTS, COMBAT, BOSS_CUTSCENE, GAME_OVER_WIN, GAME_OVER_LOSE, SNAKE_MINI_GAME,
+        ACHIEVEMENTS, COMBAT, BOSS_CUTSCENE, ENEMY_CUTSCENE, GAME_OVER_WIN, GAME_OVER_LOSE, SNAKE_MINI_GAME,
         BIRD_MINI_GAME
     }
 
