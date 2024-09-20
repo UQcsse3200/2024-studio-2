4daf39fc7b05c69596fca1b85e791f577d89448e
Sloth <long240201@gmail.com>
2024-09-20 13:48:44 +1000

    Add MiniMapDisplay class

---
+++ source/core/src/main/com/csse3200/game/areas/MiniMapDisplay.java
@@ -0,0 +1,22 @@
+package com.csse3200.game.areas;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.ui.UIComponent;
+
+public class MiniMapDisplay extends UIComponent {
+
+
+    @Override
+    public void update() {
+        //Update green point position (player in minimap)
+
+        //Update red points position (enemies in minimap)
+
+        //Update big red points position (boss in minimap)
+
+    }
+    @Override
+    protected void draw(SpriteBatch batch) {
+
+    }
+}
