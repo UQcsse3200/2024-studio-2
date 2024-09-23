c226d19d7cc9081fb8eb72e70b68bed10b5954e0
JonnaHerman <iamfeliciakeh0925@gmail.com>
2024-09-23 23:36:58 +1000

    modified OceanGameAreaConfig.java

--- source/core/src/main/com/csse3200/game/areas/OceanGameAreaConfigs/OceanMapTiles.java
+++ source/core/src/main/com/csse3200/game/areas/OceanGameAreaConfigs/OceanMapTiles.java
@@ -3,6 +3,6 @@ package com.csse3200.game.areas.OceanGameAreaConfigs;
 import com.csse3200.game.areas.OceanGameAreaConfigs.OceanTileConfig;
 
 public class OceanMapTiles {
-    public OceanTileConfig[] forestMapTiles;
+    public OceanTileConfig[] OceanMapTiles;
     public OceanTileConfig[] OceanMapTiles1;
 }
