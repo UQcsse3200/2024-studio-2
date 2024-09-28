215a50c8b45e9c15fdc1274b879fdefe72d74946
James King <jakingy.info@gmail.com>
2024-09-28 12:58:24 +1000

    Add comments to Lighting Utils

--- source/core/src/main/com/csse3200/game/lighting/LightingUtils.java
+++ source/core/src/main/com/csse3200/game/lighting/LightingUtils.java
@@ -2,7 +2,18 @@ package com.csse3200.game.lighting;
 
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * Some helpful utils to work with colors and lighting.
+ */
 public class LightingUtils {
+    /**
+     * Interpolate a colour from a wheel of colours. The colours are equally spaced around the
+     * wheel starting at x = 0.0 and wrapping back around at x = 1.0
+     *
+     * @param colors Array of colors on the wheel.
+     * @param x Position on color wheel.
+     * @return Interpolated color.
+     */
     public static Color interpolateColorCycle(Color[] colors, float x) {
         int keyPrevious = (int) Math.floor(x * colors.length);
         int keyNext = (keyPrevious + 1) % colors.length;
