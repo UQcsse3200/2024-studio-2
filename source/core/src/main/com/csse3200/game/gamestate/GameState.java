ba29eb4c9de81b260da9dfbdb42da1131bb01077
0NATE4 <navini2004@gmail.com>
2024-09-30 16:05:12 +1000

    Added in highscore saving

--- source/core/src/main/com/csse3200/game/gamestate/GameState.java
+++ source/core/src/main/com/csse3200/game/gamestate/GameState.java
@@ -1,9 +1,6 @@
 package com.csse3200.game.gamestate;
 
-import com.csse3200.game.gamestate.data.InventorySave;
-import com.csse3200.game.gamestate.data.PlayerSave;
-import com.csse3200.game.gamestate.data.QuestSave;
-import com.csse3200.game.gamestate.data.StatSave;
+import com.csse3200.game.gamestate.data.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,18 +20,17 @@ public class GameState {
 
     public static StatSave stats = new StatSave();
 
+    public static MinigameHighscore minigame = new MinigameHighscore();
+
     /**
      * Clears the contents of the GameState
      */
     public static void resetState() {
-        Logger logger = LoggerFactory.getLogger(GameState.class);
-        logger.info("state is reset");
         SaveHandler.load(GameState.class, "defaultsaves");
     }
 
     public static boolean checkState() {
-        Logger logger = LoggerFactory.getLogger(GameState.class);
-        logger.info("states: {} {} {} {}", quests, inventory, player, stats);
-        return (quests == null || inventory == null || player == null || stats == null);
+        return (quests == null || inventory == null || player == null
+                || stats == null || minigame == null);
     }
 }
