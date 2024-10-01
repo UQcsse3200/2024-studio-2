dc89e68cef707dbb92daf6d4e2be01c2ef7b4da8
yessica-s <153290587+yessica-s@users.noreply.github.com>
2024-10-02 01:18:25 +1000

    refactored combatscreentest class back to combat instead of screen tests

---
+++ source/core/src/test/com/csse3200/game/combat/CombatScreenTest.java
@@ -0,0 +1,48 @@
+package com.csse3200.game.combat;
+
+import com.badlogic.gdx.Screen;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.factories.BossFactory;
+import com.csse3200.game.entities.factories.EnemyFactory;
+import com.csse3200.game.screens.CombatScreen;
+import com.csse3200.game.screens.MainMenuScreen;
+import com.csse3200.game.services.ServiceContainer;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Test to make sure combat screen switches back to main screen after exiting.
+ */
+public class CombatScreenTest {
+    private GdxGame game;
+    private CombatScreen combatScreen;
+    private Screen oldScreen;
+    private ServiceContainer container;
+    private Entity player;
+    private Entity enemy;
+
+    @BeforeEach
+    void setUp() {
+        // Initialise temporary game, screens, and entities.
+        this.game = new GdxGame();
+        this.oldScreen = new MainMenuScreen(game);
+        this.container = new ServiceContainer();
+        //this.player = PlayerFactory.createPlayer(game);
+        this.enemy = BossFactory.createKangaBossEntity(player);
+        this.combatScreen = new CombatScreen(game, oldScreen, container, player, enemy);
+    }
+
+    /*
+    @Test
+    void testCorrectInitialise() {
+    }
+
+    @Test
+    void testCombatExitToMainScreen() {
+    }
+
+    @Test
+    void enemyIsDisposedOnWin() {
+    }
+    */
+}
