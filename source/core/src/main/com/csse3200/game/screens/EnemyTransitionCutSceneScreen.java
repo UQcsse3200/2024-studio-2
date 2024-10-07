9a8c7faa6d1abfc6d93b37dabdabb5d0df3639eb
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-08 08:57:57 +1000

    Initial setup for EnemyTransitionCutSceneScreen class with basic structure and initialization

---
+++ source/core/src/main/com/csse3200/game/screens/EnemyTransitionCutSceneScreen.java
@@ -0,0 +1,35 @@
+package com.csse3200.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.services.ServiceLocator;
+
+public class EnemyTransitionCutSceneScreen extends ScreenAdapter {
+    private final GdxGame game;
+    private final Stage stage;
+    private Skin skin;
+
+    public EnemyTransitionCutSceneScreen(GdxGame game) {
+        this.game = game;
+        this.stage = ServiceLocator.getRenderService().getStage();
+        initializeSkin();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        // Dispose of any resources
+        stage.dispose();
+    }
+
+    private void initializeSkin() {
+        skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
+    }
