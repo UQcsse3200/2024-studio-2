31fb444631d3562391a2d266fb02649c525c4d01
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-08 09:28:00 +1000

    the enemy cut scene transition screen now takes the background image

--- source/core/src/main/com/csse3200/game/screens/EnemyTransitionCutSceneScreen.java
+++ source/core/src/main/com/csse3200/game/screens/EnemyTransitionCutSceneScreen.java
@@ -3,6 +3,8 @@ package com.csse3200.game.screens;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.csse3200.game.GdxGame;
@@ -17,6 +19,8 @@ public class EnemyTransitionCutSceneScreen extends ScreenAdapter {
     private final GdxGame game;
     private final Stage stage;
     private Skin skin;
+    private Texture backgroundTexture;
+    private SpriteBatch batch;
 
     /**
      * Constructor for EnemyTransitionCutSceneScreen. Initializes the screen with necessary resources,
@@ -25,43 +29,54 @@ public class EnemyTransitionCutSceneScreen extends ScreenAdapter {
     public EnemyTransitionCutSceneScreen(GdxGame game) {
         this.game = game;
         this.stage = ServiceLocator.getRenderService().getStage();
+        this.batch = new SpriteBatch();
         initializeSkin();
+        initializeBackground();
     }
+
     /**
-    * Resizes the screen, adjusting the viewport of the stage to match the new width and height.
-    */
+     * Resizes the screen, adjusting the viewport of the stage to match the new width and height.
+     */
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
     }
 
     /**
-     * Disposes of resources when  screen is no longer needed.
+     * Disposes of resources when screen is no longer needed.
      */
-
     @Override
     public void dispose() {
-        // Dispose of any resources
+        // Dispose of  resources
         stage.dispose();
+        backgroundTexture.dispose();
+        batch.dispose();
+        skin.dispose();
     }
 
     private void initializeSkin() {
         skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
     }
 
+    private void initializeBackground() {
+        backgroundTexture = new Texture(Gdx.files.internal("images/BackgroundSplashBasic.png"));
+    }
+
     @Override
     public void show() {
-        // Create and display the enemy transition cutscene when the screen is shown
+        // This method is called when this screen becomes the current screen
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 1, 0, 1);
+        // Begin drawing the background texture
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
 
         // Update and draw the stage
         stage.act(delta);
         stage.draw();
     }
-
 }
