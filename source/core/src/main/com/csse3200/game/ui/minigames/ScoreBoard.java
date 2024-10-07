b5b11b77fea344156e01fde1a100dbb523b249e3
rp3002 <s4732400@student.edu.uq.au>
2024-10-07 14:09:09 +1000

    Modified the scoreboards.

--- source/core/src/main/com/csse3200/game/ui/minigames/ScoreBoard.java
+++ source/core/src/main/com/csse3200/game/ui/minigames/ScoreBoard.java
@@ -23,10 +23,6 @@ public class ScoreBoard {
 
     private Label scoreLabel;
     private Label highscoreLabel;
-    private Label bronzeLabel;
-    private Label silverLabel;
-    private Label goldLabel;
-    private Label medalLabel;
     private Table table;
     private final MiniGameNames gameName;
     private final int initialScore;
@@ -48,26 +44,14 @@ public class ScoreBoard {
     private void scoreBoardSetUp () {
 
         // Assign thresholds depending on game
-        int bronzeThreshold;
-        int silverThreshold;
-        int goldThreshold;
         if (gameName == SNAKE) {
             this.scale = 1;
-            bronzeThreshold = MiniGameConstants.SNAKE_BRONZE_THRESHOLD;
-            silverThreshold = MiniGameConstants.SNAKE_SILVER_THRESHOLD;
-            goldThreshold = MiniGameConstants.SNAKE_GOLD_THRESHOLD;
             this.highscore = GameState.minigame.getHighScore("snake");
         } else if (gameName == BIRD) {
-            this.scale = 0.6;
-            bronzeThreshold = MiniGameConstants.BIRDIE_DASH_BRONZE_THRESHOLD;
-            silverThreshold = MiniGameConstants.BIRDIE_DASH_SILVER_THRESHOLD;
-            goldThreshold = MiniGameConstants.BIRDIE_DASH_GOLD_THRESHOLD;
+            this.scale = 1;
             this.highscore =  GameState.minigame.getHighScore("bird");
         } else { // MAZE
             this.scale = 1;
-            bronzeThreshold = MiniGameConstants.MAZE_BRONZE_THRESHOLD;
-            silverThreshold = MiniGameConstants.MAZE_SILVER_THRESHOLD;
-            goldThreshold = MiniGameConstants.MAZE_GOLD_THRESHOLD;
             this.highscore = GameState.minigame.getHighScore("maze");
         }
 
@@ -85,23 +69,6 @@ public class ScoreBoard {
         highscoreLabel.setColor(com.badlogic.gdx.graphics.Color.WHITE);
         highscoreLabel.setAlignment(com.badlogic.gdx.utils.Align.left);
 
-        medalLabel = new Label("Medals", skin, "default-white");
-        medalLabel.setFontScale((float) (2.0f * scale));
-        medalLabel.setColor(com.badlogic.gdx.graphics.Color.WHITE);
-        medalLabel.setAlignment(com.badlogic.gdx.utils.Align.left);
-
-        bronzeLabel = new Label("Bronze: " + bronzeThreshold, skin, "default-white");
-        bronzeLabel.setFontScale((float) (1.5f * scale));
-        bronzeLabel.setAlignment(com.badlogic.gdx.utils.Align.left);
-
-        silverLabel = new Label("Silver: " + silverThreshold, skin, "default-white");
-        silverLabel.setFontScale((float) (1.5f * scale));
-        silverLabel.setAlignment(com.badlogic.gdx.utils.Align.left);
-
-        goldLabel = new Label("Gold: " + goldThreshold, skin, "default-white");
-        goldLabel.setFontScale((float) (1.5f * scale));
-        goldLabel.setAlignment(com.badlogic.gdx.utils.Align.left);
-
         table = new Table();
         table.top().right();
 
@@ -109,13 +76,6 @@ public class ScoreBoard {
         table.row();
         table.add(highscoreLabel).center().padTop(20).padBottom(10).expandX().fillX().padLeft(120);
         table.row();
-        table.add(medalLabel).center().padTop(20).padBottom(20).expandX().fillX().padLeft(120);
-        table.row();
-        table.add(bronzeLabel).center().padTop(20).padBottom(20).expandX().fillX().padLeft(120);
-        table.row();
-        table.add(silverLabel).center().padTop(20).padBottom(20).expandX().fillX().padLeft(120);
-        table.row();
-        table.add(goldLabel).center().padTop(20).padBottom(20).expandX().fillX().padLeft(120);
 
         // Set the background image
         table.setBackground(new TextureRegionDrawable(new Texture(Gdx.files.internal("images/minigames/scoreboard.png"))));
@@ -148,9 +108,6 @@ public class ScoreBoard {
         if (table != null) table.remove();
         if (scoreLabel != null) scoreLabel.remove();
         if (highscoreLabel != null) highscoreLabel.remove();
-        if (bronzeLabel != null) bronzeLabel.remove();
-        if (silverLabel != null) silverLabel.remove();
-        if (goldLabel != null) goldLabel.remove();
     }
 
     /**
@@ -170,30 +127,18 @@ public class ScoreBoard {
         float scaleFactor = Math.min(scaleFactorX, scaleFactorY);
 
         // Scale the table's size and position based on screen dimensions
-        table.setSize((float) (screenWidth * 0.22f * scale), ((float) (screenHeight * 0.5f * scale)));
+        table.setSize((float) (screenWidth * 0.3f * scale), ((float) (screenHeight * 0.2f * scale)));
         //table.setPosition(0, screenHeight - table.getHeight() - 15); // top right of screen
-        table.setPosition(screenWidth - table.getWidth() - 20,
+        table.setPosition(screenWidth - table.getWidth() + 30,
                 screenHeight - table.getHeight() - 25);
 
         // Adjust padding and font sizes within the table for the scaling factor
         scoreLabel.setFontScale(2.0f * scaleFactor);
         highscoreLabel.setFontScale(2.0f * scaleFactor);
-        medalLabel.setFontScale(2.0f * scaleFactor);
-        bronzeLabel.setFontScale(1.5f * scaleFactor);
-        silverLabel.setFontScale(1.5f * scaleFactor);
-        goldLabel.setFontScale(1.5f * scaleFactor);
-
         table.clear();
-        table.add(scoreLabel).center().padTop(100 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
+        table.add(scoreLabel).center().padTop(60 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
         table.row();
         table.add(highscoreLabel).center().padTop(20 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
         table.row();
-        table.add(medalLabel).center().padTop(20 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
-        table.row();
-        table.add(bronzeLabel).center().padTop(20 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
-        table.row();
-        table.add(silverLabel).center().padTop(20 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
-        table.row();
-        table.add(goldLabel).center().padTop(20 * scaleFactor).padBottom(10 * scaleFactor).expandX().fillX().padLeft(120 * scaleFactor);
     }
 }
