0db70f77f30da5f4194d0b2b964ec0b119cde9d2
Phong Nguyen <106220789+PHONGNGYUEN1@users.noreply.github.com>
2024-10-08 10:26:15 +1000

    udpated what enemies can be spawned in certain areas #3 #546

--- source/core/src/main/com/csse3200/game/areas/OceanGameAreaConfigs/OceanGameAreaConfig.java
+++ source/core/src/main/com/csse3200/game/areas/OceanGameAreaConfigs/OceanGameAreaConfig.java
@@ -6,12 +6,15 @@ import com.csse3200.game.files.FileLoader;
 public class OceanGameAreaConfig {public SoundsConfig sounds;
     public EntitySpawnConfig spawns;
     public TexturesConfig textures;
+    public TexturesConfig spawnsUnlocked;
 
     public OceanGameAreaConfig() {
         textures = FileLoader.readClass(TexturesConfig.class,
                 "configs/OceanGameAreaConfigs/textures.json");
         spawns = FileLoader.readClass(EntitySpawnConfig.class,
                 "configs/OceanGameAreaConfigs/entitySpawn.json");
+        spawnsUnlocked = FileLoader.readClass(TexturesConfig.class,
+                "configs/OceanGameAreaConfigs/texturesLocked.json");
         sounds = FileLoader.readClass(SoundsConfig.class,
                 "configs/OceanGameAreaConfigs/sounds.json");
     }
