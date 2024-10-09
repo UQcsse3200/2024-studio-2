fb8f55838630b2b7aa4fc0cca2d55ee99e0b8b4d
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-09 21:31:55 +1000

    replaced the switch statement by if statements as suggested by SonarCloud in MapHandler

--- source/core/src/main/com/csse3200/game/areas/MapHandler.java
+++ source/core/src/main/com/csse3200/game/areas/MapHandler.java
@@ -139,13 +139,13 @@ public class MapHandler {
    * @return map
    */
   public static GameArea getMap(MapType mapType) {
-    switch (mapType) {
-      case FOREST:
-        return (ForestGameArea) currentGameArea;
-      default:
-        throw new IllegalArgumentException("Map type not supported: " + mapType);
+    if (mapType == MapType.FOREST) {
+      return (ForestGameArea) currentGameArea;
+    } else {
+      throw new IllegalArgumentException("Map type not supported: " + mapType);
     }
   }
+
 
   /**
    * Map types
