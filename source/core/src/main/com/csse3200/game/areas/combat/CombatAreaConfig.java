476406691c827271ba78ee567a159128ad7069e1
lucas ryall <s4747956@student.uq.edu.au>
2024-10-11 13:16:14 +1000

    fixing bear so that it doesn't crash the game when starting combat

--- source/core/src/main/com/csse3200/game/areas/combat/CombatAreaConfig.java
+++ source/core/src/main/com/csse3200/game/areas/combat/CombatAreaConfig.java
@@ -67,7 +67,8 @@ public class CombatAreaConfig {
             "images/flipped_fireball.png",
             "images/enemy-chicken.png",
             "images/frog.png",
-            "images/bear.png"
+            "images/bear.png",
+            "images/enemy-bear.png"
     };
 
     static final String[] forestTextureAtlases = {
@@ -79,7 +80,7 @@ public class CombatAreaConfig {
             "images/water_boss.atlas", "images/air_boss.atlas", "images/joey.atlas",
             "images/bigsawfish.atlas", "images/macaw.atlas","images/enemy-chicken.atlas",
             "images/enemy-frog.atlas", "images/enemy-monkey.atlas", "images/bear.atlas", "images/bee.atlas",
-            "images/bigsawfish.atlas", "images/macaw.atlas", "images/eel.atlas", "images/pigeon.atlas"
+            "images/bigsawfish.atlas", "images/macaw.atlas", "images/eel.atlas", "images/pigeon.atlas", "images/enemy-bear.atlas"
     };
 
     static final String[] questSounds = {"sounds/QuestComplete.wav"};
