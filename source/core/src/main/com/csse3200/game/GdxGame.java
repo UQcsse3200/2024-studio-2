0fe54ba1d25959f7d7320449808f091efcd86ad4
jt-09 <jay.thakkar0514@gmail.com>
2024-10-13 01:57:57 +1000

    Fixed minigame crashing bug due to InGameTime not instantiated:

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -1,5 +1,6 @@
 package com.csse3200.game;
 
+import box2dLight.RayHandler;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -8,8 +9,11 @@ import com.csse3200.game.entities.Entity;
 import com.csse3200.game.files.FileLoader;
 import com.csse3200.game.gamestate.Achievements;
 import com.csse3200.game.gamestate.SaveHandler;
+import com.csse3200.game.lighting.DayNightCycle;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.screens.*;
+import com.csse3200.game.services.GameTime;
+import com.csse3200.game.services.InGameTime;
 import com.csse3200.game.services.ServiceContainer;
 import com.csse3200.game.services.ServiceLocator;
 import org.slf4j.Logger;
@@ -55,6 +59,34 @@ public class GdxGame extends Game {
         UserSettings.Settings settings = UserSettings.get();
         UserSettings.applySettings(settings);
     }
+
+    public void initializeServices() {
+        // Register GameTime
+        try {
+            ServiceLocator.getTimeSource();
+        } catch (IllegalStateException e) {
+            ServiceLocator.registerTimeSource(new GameTime());
+        }
+
+        // Register InGameTime
+        try {
+            ServiceLocator.getInGameTime();
+        } catch (IllegalStateException e) {
+            ServiceLocator.registerInGameTime(new InGameTime());
+        }
+        // Register InGameTime
+        try {
+            ServiceLocator.getDayNightCycle();
+        } catch (IllegalStateException e) {
+            // Initialize RayHandler (or mock if necessary)
+            RayHandler rayHandler = new RayHandler(null); // Pass appropriate argument
+            ServiceLocator.registerDayNightCycle(new DayNightCycle(rayHandler));
+        }
+
+        // Register other services as needed
+        // e.g., EntityService, RenderService, etc.
+    }
+
 
     /**
      * Sets the game's screen to a new screen of the provided type.
@@ -111,14 +143,17 @@ public class GdxGame extends Game {
     }
 
     public void enterSnakeScreen() {
+        initializeServices();
         addScreen(ScreenType.SNAKE_MINI_GAME, getScreen(), null, null);
     }
 
     public void enterBirdieDashScreen() {
+        initializeServices();
         addScreen(ScreenType.BIRD_MINI_GAME, getScreen(), null, null);
     }
 
     public void enterMazeGameScreen() {
+        initializeServices();
         addScreen(ScreenType.MAZE_MINI_GAME, getScreen(), null, null);
     }
 
