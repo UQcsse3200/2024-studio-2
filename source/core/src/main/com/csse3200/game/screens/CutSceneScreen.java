a349cb33c8904a63d69ed7a4e3596d816181d8d8
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-15 11:37:04 +1000

    added text label on cut scene screen

--- source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
+++ source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
@@ -1,12 +1,18 @@
 package com.csse3200.game.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound; // Import Sound
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.rendering.RenderService;
@@ -15,10 +21,6 @@ import com.csse3200.game.ui.CustomButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Represents the cutscene screen that displays
- * a cutscene before transitioning to the next game state.
- */
 public class CutSceneScreen extends ScreenAdapter {
     private static final Logger logger = LoggerFactory.getLogger(CutSceneScreen.class);
     private final GdxGame game;
@@ -30,7 +32,9 @@ public class CutSceneScreen extends ScreenAdapter {
     private Stage stage;
     private Texture cutSceneTexture;
     private Skin skin;
-    private Sound cutSceneSound; // Declare Sound variable
+    private Sound cutSceneSound;
+    private BitmapFont font;
+    private Label label;
 
     public CutSceneScreen(GdxGame game) {
         this.game = game;
@@ -70,28 +74,37 @@ public class CutSceneScreen extends ScreenAdapter {
         renderService.getStage().getViewport().update(width, height, true);
     }
 
-    /**
-     * Loads assets required for the cutscene.
-     */
     private void loadAssets() {
         logger.debug("Loading CutScene assets");
         resourceService.loadAll();
-        cutSceneTexture = new Texture(Gdx.files.internal("sounds/animal/Untitled design.png" +
-                ""));
-        cutSceneSound = Gdx.audio.newSound(Gdx.files.internal("sounds/animal/harry_potter_theme.mp3")); // Load the sound
+        cutSceneTexture = new Texture(Gdx.files.internal("sounds/animal/Untitled design.png"));
+        cutSceneSound = Gdx.audio.newSound(Gdx.files.internal("sounds/animal/harry_potter_theme.mp3"));
+
+        // Load the font and set its color to white
+        font = new BitmapFont(Gdx.files.internal("default.fnt"));
+        font.setColor(Color.WHITE); // Set text color to white
     }
 
-    /**
-     * Creates the cutscene UI elements.
-     */
     private void createCutScene() {
         logger.debug("Creating CutScene UI");
 
+
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.font = font;
+
+        // Create the label with text
+        label = new Label("Hi", labelStyle);
+        label.setFontScale(2); // Set the font size
+        label.setAlignment(Align.center);
+
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(label).expandX().padTop(50);
+
+        stage.addActor(table);
     }
 
-    /**
-     * Creates the continue button for proceeding to the next screen.
-     */
     private void createContinueButton() {
         logger.debug("Creating Continue button");
         skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
@@ -121,6 +134,7 @@ public class CutSceneScreen extends ScreenAdapter {
         spriteBatch.dispose();
         stage.dispose();
         cutSceneTexture.dispose();
-        cutSceneSound.dispose(); // Dispose of the sound
+        cutSceneSound.dispose();
+        font.dispose(); // Dispose of the font
     }
 }
