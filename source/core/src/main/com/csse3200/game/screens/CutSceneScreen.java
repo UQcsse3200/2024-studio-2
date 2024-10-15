a45d3fe3f05a23c6cf070d729ae2c263358062f0
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-15 11:52:02 +1000

    made the text fit into the screen

--- source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
+++ source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
@@ -88,25 +88,30 @@ public class CutSceneScreen extends ScreenAdapter {
     private void createCutScene() {
         logger.debug("Creating CutScene UI");
 
-
         LabelStyle labelStyle = new LabelStyle();
         labelStyle.font = font;
 
-        // Create the label with text
-        label = new Label("Once upon a time, after humans mysteriously vanished, the animal kingdoms rose again!  \n With nature reclaiming the land, every creature big and small is determined to rule it all!\\n\" +\n" +
-                "                \"In this exciting adventure, you are one of them! Choose your animal wisely, gather resources, and grow your strength. +\\n\" Will you lead your kingdom to victory and conquer them all?\\n\" +\n" +
-                "                \"From the peaceful forests to the deep oceans and beyond, other animal kingdoms are standing in your way! +\\n\"But be warned... +\\n\" the final challenge lies with the last remnants of humanity. \\n\" +\n" +
-                "                \"It’s time to unite the wild!  Let the battle for the kingdoms begin", labelStyle);
-        label.setFontScale(2); // Set the font size
+        // Create the label with the cutscene text
+        label = new Label("Once upon a time, after humans mysteriously vanished, the animal kingdoms rose again! " +
+                "With nature reclaiming the land, every creature big and small is determined to rule it all!\n" +
+                "In this exciting adventure, you are one of them! Choose your animal wisely, gather resources, and grow your strength. " +
+                "Will you lead your kingdom to victory and conquer them all?\n" +
+                "From the peaceful forests to the deep oceans and beyond, other animal kingdoms are standing in your way! " +
+                "But be warned... the final challenge lies with the last remnants of humanity.\n" +
+                "It’s time to unite the wild! Let the battle for the kingdoms begin.", labelStyle);
+
+        label.setFontScale(1.0f);
         label.setAlignment(Align.center);
+        label.setWrap(true);
 
 
         Table table = new Table();
         table.setFillParent(true);
-        table.top().padTop(20);
+        table.center().pad(50);
 
-        // Add the label to the table
-        table.add(label).expandX().align(Align.top).padTop(100);
+
+        table.add(label).expandX().width(Gdx.graphics.getWidth() - 100).center();
+
 
         stage.addActor(table);
     }
