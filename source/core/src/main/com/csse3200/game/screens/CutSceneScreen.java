c9203414f592188ff07c62bd3888a7f345d1a086
shaivikaaaa <shaivika.anand@gmail.com>
2024-10-15 10:31:56 +1000

    integrated the sound as background when cut scene screen appears

--- source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
+++ source/core/src/main/com/csse3200/game/screens/CutSceneScreen.java
@@ -1,5 +1,7 @@
 package com.csse3200.game.screens;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound; // Import Sound
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,7 +11,7 @@ import com.csse3200.game.GdxGame;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.rendering.RenderService;
 import com.csse3200.game.services.ResourceService;
-import com.csse3200.game.ui.CustomButton; // Import CustomButton
+import com.csse3200.game.ui.CustomButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +30,7 @@ public class CutSceneScreen extends ScreenAdapter {
     private Stage stage;
     private Texture cutSceneTexture;
     private Skin skin;
+    private Sound cutSceneSound; // Declare Sound variable
 
     public CutSceneScreen(GdxGame game) {
         this.game = game;
@@ -46,6 +49,9 @@ public class CutSceneScreen extends ScreenAdapter {
         createContinueButton();
 
         Gdx.input.setInputProcessor(stage);
+
+        // Play the sound when the screen is created
+        cutSceneSound.play();
     }
 
     @Override
@@ -71,6 +77,7 @@ public class CutSceneScreen extends ScreenAdapter {
         logger.debug("Loading CutScene assets");
         resourceService.loadAll();
         cutSceneTexture = new Texture(Gdx.files.internal("images/BackgroundSplashBasic.png"));
+        cutSceneSound = Gdx.audio.newSound(Gdx.files.internal("sounds/animal/harry_potter_theme.mp3")); // Load the sound
     }
 
     /**
@@ -78,25 +85,21 @@ public class CutSceneScreen extends ScreenAdapter {
      */
     private void createCutScene() {
         logger.debug("Creating CutScene UI");
-        // Logic for setting up the cutscene can go here
+
     }
 
     /**
      * Creates the continue button for proceeding to the next screen.
      */
-
     private void createContinueButton() {
         logger.debug("Creating Continue button");
         skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
 
-
         CustomButton continueButton = new CustomButton("Continue", skin);
-
 
         float buttonWidth = 200;
         float buttonHeight = 60;
         continueButton.setButtonSize(buttonWidth, buttonHeight);
-
 
         continueButton.setPosition(stage.getWidth() / 2 - buttonWidth / 2, 30);
 
@@ -108,7 +111,6 @@ public class CutSceneScreen extends ScreenAdapter {
         stage.addActor(continueButton);
     }
 
-
     @Override
     public void dispose() {
         logger.debug("Disposing CutScene screen");
@@ -118,6 +120,6 @@ public class CutSceneScreen extends ScreenAdapter {
         spriteBatch.dispose();
         stage.dispose();
         cutSceneTexture.dispose();
+        cutSceneSound.dispose(); // Dispose of the sound
     }
 }
-
