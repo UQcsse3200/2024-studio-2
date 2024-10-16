2bcfc34c54bcb5591c4c07303bce4e5e2cc92db9
Aaradheya <aaradheya.sharma@gmail.com>
2024-10-16 13:38:21 +1000

    Updated Player Icons

--- source/core/src/main/com/csse3200/game/components/gamearea/GameAreaDisplay.java
+++ source/core/src/main/com/csse3200/game/components/gamearea/GameAreaDisplay.java
@@ -3,12 +3,15 @@ package com.csse3200.game.components.gamearea;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.gamestate.GameState;
 import com.csse3200.game.ui.UIComponent;
+import com.csse3200.game.ui.pop_up_dialog_box.PopUpHelper;
 
 /**
  * Displays the player icon and a larger minimap frame based on the player's image corresponding to its kingdom.
@@ -18,6 +21,7 @@ public class GameAreaDisplay extends UIComponent {
     private Label title;
     private Texture playerIconTexture;
     private Texture minimapFrameTexture;
+    private PopUpHelper popUpHelper;  // Add PopUpHelper
 
     /**
      * Constructs a GameAreaDisplay component.
@@ -34,6 +38,7 @@ public class GameAreaDisplay extends UIComponent {
     @Override
     public void create() {
         super.create();
+        popUpHelper = new PopUpHelper(skin, stage);  // Initialize PopUpHelper
         addActors();
     }
 
@@ -72,6 +77,14 @@ public class GameAreaDisplay extends UIComponent {
         Image playerIcon = new Image(playerIconTexture);
         Image minimapFrame = new Image(minimapFrameTexture);
 
+        // Add ClickListener to playerIcon
+        playerIcon.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                showAnimalStats();
+            }
+        });
+
         // Set the size of the icons
         float titleHeight = title.getPrefHeight();
         float playerIconScaleFactor = 5.5f;   // Size for the player icon
@@ -86,8 +99,8 @@ public class GameAreaDisplay extends UIComponent {
         topTable.setFillParent(true);
         tableTwo.setFillParent(true);
 
-        // Align the table to the top left corner
-//        topTable.top().left();
+        topTable.top().left();
+        tableTwo.bottom().left();
 
         // Add the player icon to the left side of the table
         topTable.add(playerIcon)
@@ -95,28 +108,61 @@ public class GameAreaDisplay extends UIComponent {
                 .align(Align.left | Align.top)
                 .pad(5); // Padding from the edges
 
-        topTable.top().left();
-
-        // Add space to push the title and minimap frame to their respective locations
-//        topTable.add().expandX();
-
-        // Add the title in the center
-//        topTable.add(title).align(Align.center | Align.top).pad(10);
-
-        // Add space between the title and the minimap frame
-//        topTable.add().expandX();
-
         // Add the minimap frame to the right side of the table
         tableTwo.add(minimapFrame)
                 .size(titleHeight * minimapScaleFactor, titleHeight * minimapScaleFactor)
                 .align(Align.left | Align.bottom)
                 .pad(5); // Padding from the edges
 
-        tableTwo.bottom().left();
-
         // Add the table to the stage
         stage.addActor(topTable);
         stage.addActor(tableTwo);
+    }
+
+    /**
+     * Shows the animal stats popup using PopUpHelper.
+     */
+    private void showAnimalStats() {
+        String playerImagePath = GameState.player.selectedAnimalPath;
+
+        String[] waterAnimalNames = {"Dog", "Crocodile", "Bird"};
+        String[] waterAnimalDescriptions = {
+                "Dog is loyal and protective. It's great in a forest setting.",
+                "Crocodile is strong and stealthy. It thrives in water environments.",
+                "Bird is agile and quick, perfect for sky adventures."
+        };
+
+        String selectedAnimalName;
+        String selectedAnimalDescription;
+        String selectedAnimalImage;
+
+        switch (playerImagePath) {
+            case "images/dog.png" -> {
+                selectedAnimalName = waterAnimalNames[0];
+                selectedAnimalDescription = waterAnimalDescriptions[0];
+                selectedAnimalImage = "images/dog.png";
+            }
+            case "images/croc.png" -> {
+                selectedAnimalName = waterAnimalNames[1];
+                selectedAnimalDescription = waterAnimalDescriptions[1];
+                selectedAnimalImage = "images/croc.png";
+            }
+            case "images/bird.png" -> {
+                selectedAnimalName = waterAnimalNames[2];
+                selectedAnimalDescription = waterAnimalDescriptions[2];
+                selectedAnimalImage = "images/bird.png";
+            }
+            default -> {
+                selectedAnimalName = "Unknown";
+                selectedAnimalDescription = "No description available.";
+                selectedAnimalImage = "images/dog.png"; // Default image
+            }
+        }
+
+        // Display the popup with animal stats
+        popUpHelper.displayDialog(selectedAnimalName, selectedAnimalDescription, selectedAnimalImage, 600, 400, 0, () -> {
+            // Close or change screen if needed after the popup
+        });
     }
 
     /**
