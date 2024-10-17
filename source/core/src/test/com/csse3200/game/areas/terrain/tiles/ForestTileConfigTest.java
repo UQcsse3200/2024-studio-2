044e57602b5cb59c37008506ff7b04b6fe388d05
Howard-12 <s0989175085@gmail.com>
2024-10-17 11:16:28 +1000

    fix testing

--- source/core/src/test/com/csse3200/game/areas/terrain/tiles/ForestTileConfigTest.java
+++ source/core/src/test/com/csse3200/game/areas/terrain/tiles/ForestTileConfigTest.java
@@ -1,5 +1,7 @@
 package com.csse3200.game.areas.terrain.tiles;
 
+import com.csse3200.game.areas.terrain.tiles.TileConfig;
+import com.csse3200.game.areas.terrain.tiles.ForestTileConfig;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,11 +10,11 @@ class ForestTileConfigTest {
     @Test
     void testForestMapTilesInitialization() {
         // 确保forestMapTiles数组不为null且长度大于0
-        assertNotNull(ForestTileConfig.forestMapTiles, "forestMapTiles should not be null");
-        assertTrue(ForestTileConfig.forestMapTiles.length > 0, "forestMapTiles should contain at least one tile configuration");
+        assertNotNull(ForestTileConfig.getForestMapTiles(), "forestMapTiles should not be null");
+        assertTrue(ForestTileConfig.getForestMapTiles().length > 0, "forestMapTiles should contain at least one tile configuration");
 
         // 检查每个瓦片的基本属性
-        for (ForestTileConfig.TileConfig tile : ForestTileConfig.forestMapTiles) {
+        for (TileConfig tile : ForestTileConfig.getForestMapTiles()) {
             assertNotNull(tile.id, "Tile ID should not be null");
             assertNotNull(tile.fp, "File path should not be null");
             assertNotNull(tile.edges, "Edges should not be null");
